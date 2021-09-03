5a780c067f4e2a96d4017ba2d6e1dda6e1932caf
poweller <ch3ch2oh.jiro@gmail.com>
2021-09-03 14:30:48 +0900

    ナップサック問題 演習

--- DP/0-1-KnapsackProblem.py
+++ DP/0-1-KnapsackProblem.py
@@ -10,11 +10,42 @@
 # むずいのでとりあえずパス
 # ----------------------------------------
 
-def mapl(func, iter): return list(map(func, iter))
+def init_array(i, j, val=0): return [[val]*j for _ in range(i)]
 
 # input
 N, W = map(int, input().split())
-obj = [mapl(int, input().split()) for _ in range(N)]
+value, weight = [], []
+for _ in range(N):
+    v, w = map(int, input().split())
+    value.append(v)
+    weight.append(w)
 
 # solve
-dp = [[] for _ in range()]
+def knapsack(N, W):
+    DP = init_array(N+1, W+1)
+
+    for i in range(N):
+        for sum_w in range(W+1):
+
+            # この荷物を追加する場合
+            if sum_w - weight[i] >= 0:
+                DP[i+1][sum_w] = max(
+                    DP[i+1][sum_w],
+                    DP[i+1][sum_w - weight[i]] + value[i]
+                )
+
+            # 荷物を追加しない場合
+            DP[i+1][sum_w] = max(
+                DP[i+1][sum_w],
+                DP[i][sum_w]
+            )
+
+        print(*DP, sep="\n")  # test
+        print()
+
+    # print(*DP, sep="\n")  # test
+    print(DP[-1][-1])
+
+
+if __name__ == "__main__":
+    knapsack(N, W)
