291c16a81f38e8f640aa1028afcd70df7cba4b74
kentakom1213 <kentakom1213@gmail.com>
2021-09-06 10:08:07 +0900

    モジュラ逆元

--- AtCoderPython/Practice/mod_inverse.py
+++ AtCoderPython/Practice/mod_inverse.py
@@ -1,19 +1,44 @@
+# 参考
+# https://drken1215.hatenablog.com/entry/2018/06/08/210000
+# https://qiita.com/drken/items/b97ff231e43bce50199a
+# https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+
 #     a ^ (-1) mod p
 # <=> ax + py = 1
 # を満たす (x. y) を求める。
 
-def modInv(a, p, x, y):
-    # 終了条件:
-    if p == 0:
-        return a, 1, 0
+
+### 再帰関数による実装 ###
+# ac + by = gcd(a, b) = d
+# a = qb + r ...(i)
+#
+#     (qb + r)x + by = d
+# <=> b(qx + y) + rx = d ...(ii)
+# <=> bs + rt = d
+#   (s, t) が求まったとき
+#   (x, y) = (t, s - qt)
+#
+#   終了条件
+# <=> dx + 0y = d <=> (x, y) = (1, 0) ...(iii)
+#######################
+
+def extGCD(a, b):
+    if b == 0:  # (iii)
+        return (1, 0)  # -> (x, y)
+
+    q, r = a//b, a % b  # (i)
+    s, t = extGCD(b, r)
+    return (t, s - q * t)  # -> (x, y)
 
 
+def modInv(a, p):
+    if a > p: return modInv(a % p, p)
+    if a == 1: return 1  # mod(ax, p) = 1 => x = 1
+    return p - modInv(p % a, p) * (p // a) % p
 
 
-def fib(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+# 高速 mod_inverse
+# ax + py = 1
+MOD = 1000000007
+inv = []
+
