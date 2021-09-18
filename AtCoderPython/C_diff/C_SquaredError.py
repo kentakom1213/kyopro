eba48432188410a2266dc834923324711df35c58
kentakom1213 <kentakom1213@gmail.com>
2021-09-18 20:37:07 +0900

    unexpressed

--- AtCoderPython/C_diff/C_SquaredError.py
+++ AtCoderPython/C_diff/C_SquaredError.py
@@ -2,6 +2,11 @@
 # ----------------------------------------
 # 問題
 # https://atcoder.jp/contests/abc194/tasks/abc194_c
+
+# 参考
+# https://atcoder.jp/contests/abc194/editorial/826
+
+# AC (解説)
 # ----------------------------------------
 
 # 二乗和誤差に関する問題
@@ -12,15 +17,23 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-counter = [A.count(i) for i in range(-200, 201)]  # O(N)
+# counter = [A.count(i) for i in range(-200, 201)]  # O(N) -> TLE
 # 400 * 3 * 10^5 ~= 10^8 で通らないか
-# 線形だからダメ？？
+
+A.sort()
+
+counter = [0] * 401
+now = A[0]
+for i in range(N):
+    if A[i] > now: now = A[i]
+    counter[now + 200] += 1
+
 
 # O(1)
 res = 0
-# for i in range(1, 401):
-#     for j in range(i):
-#         error = (i - j) ** 2
-#         res += error * counter[i] * counter[j]
+for i in range(1, 401):
+    for j in range(i):
+        error = (i - j) ** 2
+        res += error * counter[i] * counter[j]
 
 print(res)
