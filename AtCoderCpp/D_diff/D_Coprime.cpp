212ca548ce818f5a4483175f2384f1401cb50a9e
poweller <ch3ch2oh.jiro@gmail.com>
2021-10-01 15:57:18 +0000

    readme追記

---
+++ AtCoderCpp/D_diff/D_Coprime.cpp
@@ -0,0 +1,17 @@
+//             D - Coprime 2
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc215/tasks/abc215_d
+// ----------------------------------------
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, M; cin >> N >> M;
+    vector<int> A(N);
+    for (auto& a: A) cin >> a;
+
+    
+}
