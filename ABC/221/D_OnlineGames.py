05d62ddd52f48eec3da2d9f9fac179a28a07f768
poweller <ch3ch2oh.jiro@gmail.com>
2021-10-02 18:10:29 +0000

    C問題練習

--- ABC/221/D_OnlineGames.py
+++ ABC/221/D_OnlineGames.py
@@ -1,5 +1,41 @@
 
 # いもす法??
+# from bisect import bisect_left
+
+# N = int(input())
+# span = [tuple(map(int, input().split())) for _ in range(N)]
+
+# S = [a for a, b in span]
+# G = [a+b for a, b in span]
+
+# span_sorted = sorted(set(S + G))
+
+# span_comp = [(bisect_left(span_sorted, s), bisect_left(span_sorted, g)) for s, g in zip(S, G)]
+
+# print(span_sorted)
+# print(span_comp)
+
+# # いもす法
+# imos = [0] * len(span_sorted)
+# for s, g in span_comp:
+#     imos[s] += 1
+#     imos[g] -= 1
+
+# print(imos)
+
+# login = [0] * (N+1)
+# now = 0
+# for i, diff in enumerate(imos):
+#     now += diff
+#     if i < N-1:
+#         login[now] += span_sorted[i+1] - span_sorted[i]
+
+# print(login)
+
+
+### 頑張って通したい
+### AC
+
 from bisect import bisect_left
 
 N = int(input())
@@ -8,26 +44,34 @@ span = [tuple(map(int, input().split())) for _ in range(N)]
 S = [a for a, b in span]
 G = [a+b for a, b in span]
 
-span_sorted = sorted(set(S + G))
+span_sorted = sorted(S + G)
 
 span_comp = [(bisect_left(span_sorted, s), bisect_left(span_sorted, g)) for s, g in zip(S, G)]
 
-print(span_sorted)
-print(span_comp)
+# print(span_sorted)
+# print(span_comp)
+
+span_len = len(span_sorted)
 
 # いもす法
-res = [0] * len(span_sorted)
+imos = [0] * span_len
 for s, g in span_comp:
-    res[s] += 1
-    res[g] -= 1
+    imos[s] += 1
+    imos[g] -= 1
 
-print(res)
+# print(imos)
 
-login = [0] * (N+1)
-now = 0
-for i, diff in enumerate(res):
-    now += diff
-    if i < N-1:
-        login[now] += span_sorted[i+1] - span_sorted[i]
+acc = [0] * (span_len + 1)
+for i in range(span_len):
+    acc[i+1] = acc[i] + imos[i]
 
-print(login)
+acc = acc[1:]
+# print(acc)
+
+res = [0] * (N + 1)
+
+for i, v in enumerate(acc):
+    if i+1 < span_len:
+        res[v] += (span_sorted[i+1] - span_sorted[i])
+
+print(" ".join(map(str, res[1:])))
