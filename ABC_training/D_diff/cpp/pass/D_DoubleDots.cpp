0cd52426b807763984800ba79f9dc8437bfbccb5
kentakom1213 <kentakom1213@gmail.com>
2021-10-17 03:34:23 +0000

    精進

---
+++ ABC_training/D_diff/cpp/pass/D_DoubleDots.cpp
@@ -0,0 +1,13 @@
+//          D - .. (Double Dots)
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc168/tasks/abc168_d
+// ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    
+}
