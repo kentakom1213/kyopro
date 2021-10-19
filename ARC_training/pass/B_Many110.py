74c77e3c8df800f9f6c35795d5a33d91154082bc
kentakom1213 <kentakom1213@gmail.com>
2021-10-19 08:08:01 +0000

    茶diff

---
+++ ARC_training/pass/B_Many110.py
@@ -0,0 +1,14 @@
+#              B - Many 110
+# ----------------------------------------
+# 問題
+# https://atcoder.jp/contests/arc110/tasks/arc110_b
+
+# 解説読んだけど自分でできる気がしない
+# ----------------------------------------
+
+if __name__ == "__main__":
+    N = int(input())
+    T = input()
+
+    sample = "110" * (N // 3 + 1)
+    print(sample)
