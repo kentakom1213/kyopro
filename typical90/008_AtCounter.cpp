ab8b4e39d7d17e2386991cda5e9e4722c07cb679
kentakom1213 <kentakom1213@gmail.com>
2021-10-21 15:33:37 +0000

    typical90 途中

---
+++ typical90/008_AtCounter.cpp
@@ -0,0 +1,13 @@
+//         008 - AtCounter（★4）
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/typical90/tasks/typical90_h
+// ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    
+}
