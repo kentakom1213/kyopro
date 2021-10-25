705100bb488f077127c807dbaa88da434284ae59
kentakom1213 <kentakom1213@gmail.com>
2021-10-25 14:47:07 +0000

    typical90 022

---
+++ typical90/022_CubicCake.cpp
@@ -0,0 +1,13 @@
+//            022 - Cubic Cake
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/typical90/tasks/typical90_v
+// ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    
+}
