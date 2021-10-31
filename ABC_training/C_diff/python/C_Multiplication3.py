d7a23060752b3f0e833c2e98aec60155b6adc3c7
kentakom1213 <kentakom1213@gmail.com>
2021-10-31 05:05:39 +0000

    multiplication3

--- ABC_training/C_diff/python/C_Multiplication3.py
+++ ABC_training/C_diff/python/C_Multiplication3.py
@@ -3,7 +3,7 @@
 # 問題
 # https://atcoder.jp/contests/abc169/tasks/abc169_c
 
-# 
+# AC
 # ----------------------------------------
 
 if __name__ == "__main__":
@@ -15,7 +15,11 @@ if __name__ == "__main__":
     B_digits = int(B[0]), int(B[2]), int(B[3])
     x, y, z = map(str, (A * b for b in B_digits))
 
-    res = int(x) 
-    res += int(y[:-1]) if len(y) >= 1 else 0 
-    res += int(z[:-2]) if len(z) >= 2 else 0
+    upper = int(x)
+    upper += int(y[:-1]) if len(y) > 1 else 0
+    upper += int(z[:-2]) if len(z) > 2 else 0
+
+    lower = int(y[-1]) * 10 + int(z[-2:])
+
+    res = upper + (1 if lower >= 100 else 0)
     print(res)
