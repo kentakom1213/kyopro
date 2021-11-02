96da617df5d5299c6c9d05936b01a4b9a9a18185
kentakom1213 <kentakom1213@gmail.com>
2021-11-02 20:14:58 +0000

    グラフの問題

--- typical90/026_IndependendentSetOnATree.py
+++ typical90/026_IndependendentSetOnATree.py
@@ -2,6 +2,8 @@
 # ----------------------------------------
 # 問題
 # https://atcoder.jp/contests/typical90/tasks/typical90_z
+
+# AC
 # ----------------------------------------
 
 # 解説
@@ -11,6 +13,12 @@
 # | 変で直接結ばれた頂点同士が互いに違う色になるように塗ることができるグラフ
 
 # 木は必ず2部グラフになるから、depthが大きい順に出力すれば良い
+
+# PyPy3   -> TLE
+# Python3 -> AC
+
+import sys
+sys.setrecursionlimit(100000)
 
 def dfs(now, now_depth):
     if depth[now] != -1:
@@ -33,7 +41,9 @@ if __name__ == "__main__":
         G[a].append(b)
         G[b].append(a)
 
-    print(depth)
-
     dfs(0, 0)
-    print(depth)
+
+    if sum( d & 1 for d in depth ) > N//2:
+        print(*[i+1 for i, d in enumerate(depth) if d & 1][:N//2])
+    else:
+        print(*[i+1 for i, d in enumerate(depth) if not(d & 1)][:N//2])
