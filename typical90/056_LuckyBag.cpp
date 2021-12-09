6062b6390c72a895935cd2b67889dbbe657003c3
kentakom1213 <kentakom1213@gmail.com>
2021-12-09 07:24:33 +0000

    typical90 056 AC

--- typical90/056_LuckyBag.cpp
+++ typical90/056_LuckyBag.cpp
@@ -2,6 +2,8 @@
 // ----------------------------------------
 // 問題
 // https://atcoder.jp/contests/typical90/tasks/typical90_bd
+
+// AC
 // ----------------------------------------
 
 // 2^100 ~= 10^30 だから、全探索は不可能
@@ -9,43 +11,62 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-typedef unsigned long long ll;
-#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define ALL(A) A.begin(), A.end()
-#define initArray(name, h, w, v) vector<vector<ll>> name(h, vector<ll>(w, v));
-typedef pair<ll, ll> vec2;
-typedef vector<vector<ll>> Array;
+#define initArray(name, h, w, v) vector<vector<int>> name(h, vector<int>(w, v));
+typedef long long ll;
+typedef pair<int, int> vec2;
+typedef vector<vector<int>> Array;
 template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
 template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
 
 int main() {
-    ll N, S; cin >> N >> S;
-    vector<ll> A(N), B(N);
+    int N, S; cin >> N >> S;
+    vector<int> A(N), B(N);
     rep(i, N) cin >> A[i] >> B[i];
 
-    vector<vector<string>> dp(N+1, vector<string>(S+1, ""));
-    dp[0][0] = "X";
+    initArray(dp, N+1, S+1, 0);
+    dp[0][0] = 1;
 
-    for (ll i = 0; i < N; i++) {
-        for (ll j = 0; j < S; j++) {
-            if (dp[i][j] == "") continue;
+    // 普通のナップサック問題
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < S; j++) {
+            if (!dp[i][j]) continue;
 
             // Aを追加
             if (j + A[i] <= S) {
-                dp[i+1][j+A[i]] = dp[i][j] + 'A';  // Aのときは足さない
+                dp[i+1][j+A[i]] = dp[i][j];
             }
             // Bを追加
             if (j + B[i] <= S) {
-                dp[i+1][j+B[i]] = dp[i][j] + 'B';  // Bのときは2^iを足す
+                dp[i+1][j+B[i]] = dp[i][j];
             }
         }
     }
 
-    string res = dp[N][S];
-    if (res != "") {
-        cout << res.substr(1, N) << endl;;
-    }
-    else {
+    // 解なし
+    if (dp[N][S] == 0) {
         cout << "Impossible" << endl;
+        return 0;
     }
+
+    // dp復元
+    string res;
+    int now_sum = S;
+    for (int i = N-1; i >= 0; i--) {
+        if (now_sum - A[i] >= 0 && dp[i][now_sum - A[i]]) {
+            // printf("now_sum - A[i] = %d - %d = %d\n", now_sum, A[i], now_sum-A[i]);
+            res.push_back('A');
+            now_sum -= A[i];
+        }
+        else {
+            // printf("now_sum - B[i] = %d - %d = %d\n", now_sum, B[i], now_sum-B[i]);
+            res.push_back('B');
+            now_sum -= B[i];
+        }
+    }
+
+    reverse(ALL(res));
+    cout << res << endl;
 }
+
