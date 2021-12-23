db0476a57ba4571b966d5cc1253f35392d970567
kentakom1213 <kentakom1213@gmail.com>
2021-12-23 22:00:09 +0900

    typical90 058

---
+++ typical90/058_OriginalCalculator.py
@@ -0,0 +1,11 @@
+#  058 - Original Calculator（★4）
+#  ------------------------------
+#  問題
+#  https://atcoder.jp/contests/typical90/tasks/typical90_bf
+
+#  MOD = 100000であるから、メモ化によって高々10^5回の計算で済む
+
+n, k = map(int, input().split())
+
+MOD = 100000
+
