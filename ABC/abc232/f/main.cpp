582893087cf2354420589f9817e35482eb0a7a8d
kentakom1213 <kentakom1213@gmail.com>
2021-12-25 14:06:58 +0000

    abc233 追加

---
+++ ABC/abc232/f/main.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(A) A.begin(), A.end()
+#define initArray(name, h, w, v) vector<vector<int>> name(h, vector<int>(w, v));
+typedef long long ll;
+typedef pair<int, int> vec2;
+typedef vector<vector<int>> Array;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+
+int main() {
+    int N; cin >> N;
+    ll X, Y; cin >> X >> Y;
+    vector<ll> A(N), B(N);
+    rep(i, N) cin >> A[i];
+    rep(i, N) cin >> B[i];
+
+    
+}
