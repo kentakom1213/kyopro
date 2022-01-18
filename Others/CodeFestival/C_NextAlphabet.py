b7c7c28e41b48b5ec930256f2a59154cceb163d5
kentakom1213 <kentakom1213@gmail.com>
2022-01-19 01:05:31 +0900

    bit全探索

---
+++ Others/CodeFestival/C_NextAlphabet.py
@@ -0,0 +1,14 @@
+#          C - 次のアルファベット
+# ----------------------------------------
+# 問題
+# https://atcoder.jp/contests/code-festival-2016-quala/tasks/codefestival_2016_qualA_c
+# ----------------------------------------
+
+# 前の文字から順番に貪欲に
+
+from string import ascii_lowercase
+
+s = input()
+K = int(input())
+
+
---
+++ Sakumon/prime_birthday.cpp
@@ -0,0 +1,10 @@
+// # Prime Day
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    // 素因数となりうる値の配列
+    vector<int> divisor(10101)
+}
