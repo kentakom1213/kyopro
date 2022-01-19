4667f3c4bc14461f07c0ef7dee9b718343103386
kentakom1213 <kentakom1213@gmail.com>
2022-01-19 14:18:48 +0000

    typical90 084

--- typical90/069_ColorfulBlocks2.py
+++ typical90/069_ColorfulBlocks2.py
@@ -2,21 +2,13 @@
 # ----------------------------------------
 # 問題
 # https://atcoder.jp/contests/typical90/tasks/typical90_bq
-
 # 参考
 # https://atcoder.jp/contests/typical90/editorial/2056
 
+# AC (解説)
 # ----------------------------------------
 
-# 普通にDPで解く
-
+MOD = 1000000007
 N, K = map(int, input().split())
-
-color = [0] * N  # 使える色の種類
-dp = [0] * N  # dp[i] := i番目までの要素を使って塗れる組み合わせ
-
-color[0] = K
-dp[0] = K
-
-for i in range(1, N):
-    color[i] = color[i-1] - 1
+ans = K if N == 1 else K * (K-1) * pow(K-2, N-2, MOD) % MOD
+print(ans)
