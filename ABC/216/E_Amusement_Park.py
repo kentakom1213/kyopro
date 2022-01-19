3d4f46a575ddd961c191150cce59aec2027917cb
kentakom1213 <kentakom1213@gmail.com>
2022-01-19 13:05:47 +0000

    難しい

--- ABC/216/E_Amusement_Park.py
+++ ABC/216/E_Amusement_Park.py
@@ -1,49 +1,33 @@
-#        E - Amusement Park
-# ----------------------------------------
-# 問題
-# https://atcoder.jp/contests/abc216/tasks/abc216_e
-# ----------------------------------------
+# E - Amusement Park
+# TLE
 
-# 大きい方から貪欲に追加していく
-# 操作はN回以内に終了する
+from bisect import bisect_left
+def mapl(func, iter): return list(map(func, iter))
 
-from collections import defaultdict
-
+# input
 N, K = map(int, input().split())
-A_vals = defaultdict(int)
-for i in input().split():
-    A_vals[int(i)] += 1
+A = sorted(mapl(int, input().split()), reverse=True)
 
-A = list(A_vals.items())
-A.sort()
-
-res = 0
-while len(A) >= 2 and K:
-    print(A)
-    cur, cnt = A.pop()
-    nxt, ncnt = A.pop()  # 次に大きい値
-
-    # curからnxtになるまでresを追加
-    # 追加する回数
-    n = (cur - nxt + 1) * cnt
-
-    if n <= K:
-        res += (cur + nxt) * (cur - nxt + 1) // 2
-        A.append((nxt, ncnt + cnt))
-        K -= n
+result = 0
+while K > 0:
+    diff = A[0] - A[1]
+    if diff == 0:
+        result += A[0] * diff - diff * (diff + 1) / 2
+        K -= diff
     else:
-        A = [(nxt, ncnt), (cur, cnt)]
-        break
+        pass
 
-# Aは必ず要素を持つ
-cur, cnt = A.pop()
-n = cnt // K  # 加算できる回数
-res += (2 * cur - n) * n // 2
-K -= n
-cur -= n
 
-print(res)
-res += cur * K
+print(result)
 
-print(K)
-print(res)
+# # solve
+# result = 0
+# while K > 0 and A[-1] > 0:
+#     result += A[-1]
+#     A[-1] -= 1
+#     # A.insert(bisect_left(A, a), a)
+#     A.append(A.pop(-2))
+
+#     K -= 1
+#     # print(K, A, "->", result)  # test
+
