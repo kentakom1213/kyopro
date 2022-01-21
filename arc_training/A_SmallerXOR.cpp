c4ef8b9da7fd4b0b9636f5bd5f4ad8db436f9a6a
kentakom1213 <kentakom1213@gmail.com>
2022-01-21 16:38:57 +0000

    条件for文を実装

---
+++ arc_training/A_SmallerXOR.cpp
@@ -0,0 +1,23 @@
+//           A - Smaller XOR
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/arc129/tasks/arc129_a
+// ----------------------------------------
+
+// 
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(A) A.begin(), A.end()
+#define initArray(name, h, w, v) vector<vector<long long>> name(h, vector<long long>(w, v));
+typedef long long ll;
+typedef pair<long long, long long> vec2;
+typedef vector<vector<long long>> Array;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+
+int main() {
+    ll N, L, R; cin >> N >> L >> R;
+
+}
