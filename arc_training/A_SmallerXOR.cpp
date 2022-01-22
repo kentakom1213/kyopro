50ef5951dda69c5be70e0b8f4e1cae5b9022a946
kentakom1213 <kentakom1213@gmail.com>
2022-01-22 04:50:34 +0000

    smaller xor

--- arc_training/A_SmallerXOR.cpp
+++ arc_training/A_SmallerXOR.cpp
@@ -4,7 +4,10 @@
 // https://atcoder.jp/contests/arc129/tasks/arc129_a
 // ----------------------------------------
 
-// 
+// 解説
+// (x >> i)&1 == 1 となる最大のiについて
+//   (N >> i)&1 == 1 のとき -> (N xor x) > N
+//   (N >> i)&1 == 0 のとき -> (N xor x) < N
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -19,5 +22,19 @@ template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; r
 
 int main() {
     ll N, L, R; cin >> N >> L >> R;
+    int MAX = 0;
+    while (N >> MAX) MAX++;
+    MAX++;
 
+    // Nを2進法で表したときのi桁目が1であるとき
+    ll cnt = 0;
+    for (int i = 0; i <= MAX; i++) if ((N>>i)&1) {
+        ll max_bit = (1LL << (i+1)) - 1;
+        ll min_bit = 1LL << i;
+        ll upper = min(R, max_bit);
+        ll lower = max(L, min_bit);
+        if (lower <= upper) cnt += upper - lower + 1;
+    }
+
+    cout << cnt << endl;
 }
