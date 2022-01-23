f1a38dd213b8f883195eba4e2557daf95fcad900
kentakom1213 <kentakom1213@gmail.com>
2022-01-23 15:29:28 +0000

    bfs + dp

--- abc_training/D/D_NumberOfShortestPaths.py
+++ abc_training/D/D_NumberOfShortestPaths.py
@@ -4,6 +4,11 @@
 # https://atcoder.jp/contests/abc211/tasks/abc211_d
 # ----------------------------------------
 
+# bfs + dp
+
+from collections import deque
+MOD = 1000000007
+
 N, M = map(int, input().split())
 G = [[] for _ in range(N)]
 for i in range(M):
@@ -12,5 +17,22 @@ for i in range(M):
     G[a].append(b)
     G[b].append(a)
 
-def bfs(G, start):
-    pass
+# bfs
+path = [-1] * N
+path[0] = 0
+cnt = [0] * N
+cnt[0] = 1
+q = deque([0])
+while q:
+
+    cur = q.popleft()
+    for nxt in G[cur]:
+        if path[nxt] == -1:
+            path[nxt] = path[cur] + 1
+            cnt[nxt] = cnt[cur]
+            q.append(nxt)
+        elif path[nxt] == path[cur] + 1:
+            cnt[nxt] += cnt[cur]
+            cnt[nxt] %= MOD
+    
+print(cnt[N-1])
