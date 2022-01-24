9e4153efbab5368ef671b861f4eb18ad2f7ab885
kentakom1213 <kentakom1213@gmail.com>
2022-01-24 04:05:07 +0000

    match

---
+++ abc_training/D/D_TakahashiUnevolved.py
@@ -0,0 +1,7 @@
+#        D - Takahashi Unevolved
+# ----------------------------------------
+# 問題
+# https://atcoder.jp/contests/abc180/tasks/abc180_d
+# ----------------------------------------
+
+
