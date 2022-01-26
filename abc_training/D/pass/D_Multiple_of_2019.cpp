7913fdb2089ba959e548e659d44d190a2a813e38
kentakom1213 <kentakom1213@gmail.com>
2022-01-26 05:17:54 +0000

    E問題演習

---
+++ abc_training/D/pass/D_Multiple_of_2019.cpp
@@ -0,0 +1,23 @@
+//         D - Multiple of 2019
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc164/tasks/abc164_d
+// ----------------------------------------
+
+// S[i:j] -> iを前列挙し、jを高速に数え上げる
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(A) A.begin(), A.end()
+#define initArray(name, h, w, v) vector<vector<long long>> name(h, vector<long long>(w, v));
+typedef long long ll;
+typedef pair<long long, long long> vec2;
+typedef vector<vector<long long>> Array;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+
+int main() {
+    string S; cin >> S;
+
+}
