7d3714d442bfa297a8c059892ee26acd508d4c37
kentakom1213 <kentakom1213@gmail.com>
2022-01-29 14:27:47 +0000

    ceil(a/b) -> (a+b-1)//b

--- ARC/arc134/a/main.py
+++ ARC/arc134/a/main.py
@@ -1,5 +1,3 @@
-from math import ceil
-
 N, L, W = map(int, input().split())
 A = list(map(int, input().split())) + [L]
 
@@ -7,9 +5,7 @@ now = cnt = 0
 for x in A:
     if now < x:
         span = x - now
-        cnt += span // W
-        if span % W:
-            cnt += 1
+        cnt += (span + W - 1) // W  # 今後ceilはこれを使おう
     now = x + W
 
 print(cnt)
