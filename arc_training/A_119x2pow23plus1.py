aa5e977243b53b22847f9f6e8fb8610d17e6317c
kentakom1213 <kentakom1213@gmail.com>
2022-02-03 15:12:55 +0000

    arc - a

--- arc_training/A_119x2pow23plus1.py
+++ arc_training/A_119x2pow23plus1.py
@@ -2,8 +2,25 @@
 # ----------------------------------------
 # 問題
 # https://atcoder.jp/contests/arc119/tasks/arc119_a
+
+# ARCの灰diff絶対盛ってるでしょ...
+
+# AC
 # ----------------------------------------
+
+# N = a * 2^b + c
+# b < 60
+
+# a = N // 2**b
+# c = N - a*2^b
+# a+b+c = a + b + N - a*2^b
 
 N = int(input())
 
+min_abc = 1e30
+for b in range(61):
+    a = N // 2**b
+    c = N - a * 2**b
+    min_abc = min(min_abc, a+b+c)
 
+print(min_abc)
