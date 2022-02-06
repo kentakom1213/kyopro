043ef93cced88d1693b7877950e47331b3491c7a
kentakom1213 <kentakom1213@gmail.com>
2022-02-06 16:33:21 +0000

    速解き練習

---
+++ abc_training/D/D_Poker.py
@@ -0,0 +1,12 @@
+import queue
+#               D - Poker
+# ----------------------------------------
+# 問題
+# https://atcoder.jp/contests/abc193/tasks/abc193_d
+# ----------------------------------------
+
+
+K = int(input())
+S, T = input(), input()
+
+
