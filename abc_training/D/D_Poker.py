71a9c8df76d2a457bd6d2cd81ede34ddf2740e78
kentakom1213 <kentakom1213@gmail.com>
2022-02-07 06:50:04 +0000

    D - Poker AC

--- abc_training/D/D_Poker.py
+++ abc_training/D/D_Poker.py
@@ -8,12 +8,15 @@ import queue
 K = int(input())
 S, T = input(), input()
 
+cnt = [K] * 10
 cntA = [0] * 10
 cntB = [0] * 10
 
 for a, b in zip(S, T):
     a = int(a) if a!="#" else 0
     b = int(b) if b!="#" else 0
+    cnt[a] -= 1
+    cnt[b] -= 1
     cntA[a] += 1
     cntB[b] += 1
 
@@ -22,10 +25,10 @@ ans = 0
 for a in range(1, 10):
     for b in range(1, 10):
         # カードが出る確率
-        pA = (K - cntA[a])
-        pB = (K - cntB[b])
+        pA = cnt[a]; cnt[a] -= 1
+        pB = cnt[b]; cnt[a] += 1
 
-        # 勝敗
+        # 点数を計算
         cntA[a] += 1; cntB[b] += 1
         pointA = sum(i*10**cntA[i] for i in range(1, 10))
         pointB = sum(i*10**cntB[i] for i in range(1, 10))
@@ -34,5 +37,5 @@ for a in range(1, 10):
         if pointA > pointB:
             ans += pA * pB
 
-ans /= (9*K - 8) ** 2
+ans /= (9*K - 8) * (9*K - 9)
 print(ans)
