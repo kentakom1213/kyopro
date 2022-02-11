c8cf0d2f1d07329c0267848aaa6499a85c6bc5bd
kentakom1213 <kentakom1213@gmail.com>
2022-02-11 12:01:00 +0900

    whatsnewを追加

---
+++ abc_training/D/D_Blue_and_Red_Balls.py
@@ -0,0 +1,12 @@
+# D - Blue and Red Balls
+# -----------------------
+# 問題
+# https://atcoder.jp/contests/abc132/tasks/abc132_d
+# -----------------------
+
+# dpでとく
+
+N, K = map(int, input().split())
+
+
+
