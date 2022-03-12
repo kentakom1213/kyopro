fb347c7ce15c85141f4e634f8f94ad3cc871f4e0
kentakom1213 <kentakom1213@gmail.com>
2022-03-12 15:05:59 +0000

    木dp？？

--- abc_training/D/D_Ki.py
+++ abc_training/D/D_Ki.py
@@ -4,6 +4,10 @@
 # https://atcoder.jp/contests/abc138/tasks/abc138_d
 # ----------------------------------------
 
+import sys
+sys.setrecursionlimit(1000000)
+from collections import defaultdict
+
 N, Q = map(int, input().split())
 G = [[] for _ in range(N)]
 for _ in range(N-1):
@@ -12,6 +16,23 @@ for _ in range(N-1):
     G[a].append(b)
     G[b].append(a)
 
-queries = [tuple(map(int, input().split())) for _ in range(N)]
+queries = defaultdict(int)
+for _ in range(Q):
+    p, x = map(int, input().split())
+    queries[p-1] += x
 
+is_visited = [0] * N
+ans = [0] * N
 
+# dfs
+def dfs(cur, val):
+    is_visited[cur] = 1
+    val += queries[cur]
+    ans[cur] = val
+    for nxt in G[cur]:
+        if is_visited[nxt]:
+            continue
+        dfs(nxt, val)
+
+dfs(0, 0)
+print(*ans, sep=" ")
