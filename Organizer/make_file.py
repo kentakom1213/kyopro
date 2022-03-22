5c8ca81ad1a2e3cf7ca020a7354031a9c8a824d4
kentakom1213 <kentakom1213@gmail.com>
2022-03-22 13:09:09 +0000

    ファイル管理システムを作成

---
+++ Organizer/make_file.py
@@ -0,0 +1,105 @@
+"""
+# 問題を解くファイルの作成
+1. url、言語を入力
+2. 問題のページを取得し、分類 -> ./scrape.py で行う
+3. テンプレートをもとにファイルを作成
+"""
+
+import re
+import scrape
+from pathlib import Path
+
+problem_dir = {
+    "abc": "abc_training",
+    "arc": "arc_training",
+    "agc": "agc_training",
+    "other": "Others/others",
+    "aoj": "AOJ",
+}
+
+def make_filename(data: dict, lang: str="py") -> Path:
+    """ファイル名を作成する"""
+
+    # ディレクトリ名
+    dir = Path(__file__).parent.parent
+    
+    if data["service"] == "atcoder":
+        # コンテストで分類
+        dir /= problem_dir[data["contest_type"]]
+
+        # 問題難易度で分類
+        if data["contest_type"] in ("abc", "arc", "agc"):
+            dir /= data["problem_type"].upper()
+
+    if data["service"] == "aoj":
+        dir /= problem_dir["aoj"]
+
+    # ファイル名
+    filename = re.sub(
+        r"-|:",
+        "",
+        data["title"]
+    )
+
+    filename = re.sub(
+        r" +",
+        "_",
+        filename
+    )
+
+    filename += "." + lang
+
+    return dir / filename
+
+
+def make_header(data: dict, lang: str):
+    """C++ / Python用のヘッダーを作成"""
+
+    comment = {"py": "#", "cpp": "//"}
+    line = "-" * 40
+
+    template = [
+        data["title"].center(40),
+        line,
+        "問題",
+        data["url"],
+        line,
+    ]
+
+    result = "\n".join(
+        map(
+            lambda x: f"{comment[lang]} {x}",
+            template
+        )
+    )
+
+    return result
+
+
+def main():
+    print("Let's solve!")
+    url = input("url: ")
+    lang = {
+        "py": "py",
+        "python": "py",
+        "cpp": "cpp",
+        "c++": "cpp",
+    }[input("lang: ") or "py"]  # デフォルトは"py"
+
+    data = scrape.get_problem_info(url)
+    filename = make_filename(data, lang)
+    header = make_header(data, lang)
+
+    # ディレクトリの作成
+    filename.parent.mkdir(exist_ok=True)
+
+    # ファイルの作成
+    filename.touch()
+
+    # ヘッダーの埋め込み
+    with open(filename, "w") as f:
+        f.write(header)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Organizer/test.py
@@ -0,0 +1,86 @@
+"""
+# テスト
+scrape.py, make_file.pyのテストコード
+"""
+
+
+import scrape, make_file
+
+atcoder_url = [
+    "https://atcoder.jp/contests/abc136/tasks/abc136_a",
+    "https://atcoder.jp/contests/abc014/tasks/abc014_4",
+    "https://atcoder.jp/contests/arc134/tasks/arc134_b",
+    "https://atcoder.jp/contests/arc099/tasks/arc099_a",
+    "https://atcoder.jp/contests/agc026/tasks/agc026_b",
+    "https://atcoder.jp/contests/agc004/tasks/agc004_e",
+    "https://atcoder.jp/contests/tokiomarine2020/tasks/tokiomarine2020_a",
+    "https://atcoder.jp/contests/aising2020/tasks/aising2020_a",
+]
+
+aoj_url = [
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A",
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0537",
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B&lang=jp",
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0202",
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3246",
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2377",
+    "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0306",
+]
+
+def test_scrape():
+    print("test \"scrape.py\"")
+
+    for url in atcoder_url:
+        res = scrape.get_problem_info(url)
+        print(res)
+    
+    for url in aoj_url:
+        res = scrape.get_problem_info(url)
+        print(res)
+
+    print("OK\n")
+    
+
+def test_make_filename():
+    print("test \"make_file.py\" - make_filename")
+
+    for url in atcoder_url:
+        data = scrape.get_problem_info(url)
+        res = make_file.make_filename(data)
+        print(res)
+    
+    for url in aoj_url:
+        data = scrape.get_problem_info(url)
+        res = make_file.make_filename(data)
+        print(res)
+
+    print("OK\n")
+
+
+def test_make_header():
+    print("test \"make_file.py\" - make_header")
+
+    urls = [
+        atcoder_url[0],
+        atcoder_url[2],
+        aoj_url[3],
+        aoj_url[4]
+    ]
+
+    datas = list(map(scrape.get_problem_info, urls))
+    headers_py = map(lambda d: make_file.make_header(d, "py"), datas)
+    headers_cpp = map(lambda d: make_file.make_header(d, "cpp"), datas)
+
+    print("Template for Python")
+    print(*headers_py, sep="\n\n")
+
+    print("\nTemplate for C++")
+    print(*headers_cpp, sep="\n\n")
+
+    print("OK\n")
+
+
+if __name__ == "__main__":
+    test_scrape()
+    test_make_filename()
+    test_make_header()
