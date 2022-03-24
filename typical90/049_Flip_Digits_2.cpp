61db7278aecb2b600aa6b9970c9d7816d72ae7cf
kentakom1213 <kentakom1213@gmail.com>
2022-03-24 14:44:51 +0000

    arc

--- typical90/049_Flip_Digits_2.cpp
+++ typical90/049_Flip_Digits_2.cpp
@@ -3,3 +3,21 @@
 // 問題
 // https://atcoder.jp/contests/typical90/tasks/typical90_aw
 // ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define range(i, begin, end) for (int i = (int)(begin); i < (int)(end); i++)
+#define ALL(A) A.begin(), A.end()
+#define initArray(name, h, w, v) vector<vector<int>> name(h, vector<int>(w, v));
+typedef long long ll;
+typedef pair<int, int> vec2;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+constexpr int MOD = 1000000007;
+constexpr int mod = 998244353;
+
+int main() {
+    ll N, M; cin >> N >> M;
+    
+}
