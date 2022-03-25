d75fec2a45fb7daf5bbd8d9d2cb6e46735af64bb
kentakom1213 <kentakom1213@gmail.com>
2022-03-25 11:46:02 +0000

    緑

--- abc_training/C/C_高橋君の給料.cpp
+++ abc_training/C/C_高橋君の給料.cpp
@@ -17,36 +17,50 @@ template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; r
 constexpr int MOD = 1000000007;
 constexpr int mod = 998244353;
 
-void dfs(vector<int> &salary, const vector<int> B, int cur) {
-    bool has_buka = false;
-    int min_buka = 1, max_buka = 1;
-    for (auto b : B) {
-        if (b == cur) {
-            has_buka = true;
-            if (salary[b] == 0) dfs(salary, B, b);
+template <typename T>
+void print_vector(vector<T>& vec) {
+  cerr << "[ ";
+  for (int i = 0; i < vec.size(); i++) {
+    if (i < vec.size() - 1) cerr << vec.at(i) << " ";
+    else cerr << vec.at(i);
+  }
+  cerr << " ]" << endl;
+}
 
-            // 最小値、最大値の更新
-            chmin(min_buka, salary[b]);
-            chmax(max_buka, salary[b]);
+int N;
+vector<int> B, salary;
+
+void dfs(int cur) {
+    bool has_buka = false;
+    int min_salary=1<<30, max_salaly=1;
+
+    rep(i, N) if (i!=cur) {
+        if (cur == B[i]) {
+            has_buka = true;
+            dfs(i);
+            chmin(min_salary, salary[i]);
+            chmax(max_salaly, salary[i]);
         }
     }
+
     if (has_buka) {
-        salary[cur] = min_buka + max_buka + 1;
+        salary[cur] = min_salary + max_salaly + 1;
     } else {
         salary[cur] = 1;
     }
 }
 
 int main() {
-    int N; cin >> N;
-    vector<int> B(N), salary(N, 0);
-    rep(i, N-1) {
-        cin >> B[i+1];
+    cin >> N;
+    B.assign(N, 0);
+    salary.assign(N, 0);
+
+    range(i, 1, N) {
+        cin >> B[i];
         B[i]--;
     }
 
-    // 木DP
-    dfs(salary, B, 0);
+    dfs(0);
 
     cout << salary[0] << endl;
 }
