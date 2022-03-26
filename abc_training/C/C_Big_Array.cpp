63af7bb1e3460aaa830d6c6693877bd5a8c5d9bc
kentakom1213 <kentakom1213@gmail.com>
2022-03-26 04:30:51 +0000

    解けない

---
+++ abc_training/C/C_Big_Array.cpp
@@ -0,0 +1,37 @@
+//              C - Big Array              
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc061/tasks/abc061_c
+// ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define range(i, begin, end) for (int i = (int)(begin); i < (int)(end); i++)
+#define ALL(A) A.begin(), A.end()
+#define initArray(name, h, w, v) vector<vector<int>> name(h, vector<int>(w, v));
+typedef long long ll;
+typedef pair<int, int> vec2;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+constexpr int MOD = 1000000007;
+constexpr int mod = 998244353;
+
+int main() {
+    int N, K; cin >> N >> K;
+    map<ll, ll> arr;
+
+    while (N--) {
+        ll a, b; cin >> a >> b;
+        arr[a] += b;
+    }
+
+    ll sum = 0;
+    for (auto [k, v] : arr) {
+        if (sum + v < K) sum += v;
+        else {
+            cout << k << endl;
+            return 0;
+        }
+    }
+}
