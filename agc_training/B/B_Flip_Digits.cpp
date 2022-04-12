86506e48fa27adc30fcef9f9fe8cc8ece53eda5f
kentakom1213 <kentakom1213@gmail.com>
2022-04-12 11:38:47 +0900

    新規

---
+++ agc_training/B/B_Flip_Digits.cpp
@@ -0,0 +1,25 @@
+//             B - Flip Digits             
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/agc049/tasks/agc049_b
+// ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(A) A.begin(), A.end()
+typedef long long ll;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+constexpr int MOD = 1000000007;
+constexpr int mod = 998244353;
+
+int N;
+string S, T;
+
+int main() {
+    cin >> N >> S >> T;
+
+    // 累積xorを取る
+    
+}
