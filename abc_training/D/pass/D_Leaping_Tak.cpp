58fabf2c3042433ac397ea1de698de66bc0fa93e
kentakom1213 <kentakom1213@gmail.com>
2022-04-12 07:00:51 +0000

    01-knapsack

---
+++ abc_training/D/pass/D_Leaping_Tak.cpp
@@ -0,0 +1,21 @@
+//             D - Leaping Tak             
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc179/tasks/abc179_d
+// ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(A) A.begin(), A.end()
+typedef long long ll;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+constexpr int MOD = 1000000007;
+constexpr int mod = 998244353;
+
+ll N, K;
+
+int main() {
+    
+}
---
+++ arihon/chapter2/section3/0-1_Knapsack_Problem.cpp
@@ -0,0 +1,42 @@
+/*
+# [0-1 Knapsack Problem](https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_B&lang=jp)
+
+## result
+- AC
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(A) A.begin(), A.end()
+typedef long long ll;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+constexpr int MOD = 1000000007;
+constexpr int mod = 998244353;
+
+const int MAX_N = 111, MAX_W = 101010;
+int N, W;
+int v[MAX_N], w[MAX_W];
+int memo[MAX_N][MAX_W];
+
+int pack(int i, int rem) {
+    if (memo[i][rem] != -1) return memo[i][rem];
+    if (i == N) return 0;
+
+    // i個めを選ばない
+    int ans = pack(i+1, rem);
+    // i個めを選ぶ
+    if (w[i] <= rem) {
+        chmax(ans, v[i] + pack(i+1, rem - w[i]));
+    }
+    return memo[i][rem] = ans;
+}
+
+int main() {
+    memset(memo, -1, sizeof(memo));
+    cin >> N >> W;
+    rep(i, N) cin >> v[i] >> w[i];
+
+    cout << pack(0, W) << endl;
+}
