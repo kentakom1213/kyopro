ba9c63376c8da204764cb282f744a59c30d20200
kentakom1213 <kentakom1213@gmail.com>
2022-04-19 06:40:20 +0000

    数独ソルバ改良

--- algorithm/sudoku_solver.py
+++ algorithm/sudoku_solver.py
@@ -2,14 +2,14 @@
 def solve(arr, cell):
     """単純なdfsで解く"""
 
-    # 0が含まれていない場合
+    # 求まったとき
     if cell == 81:
         print(*arr, sep="\n")
-        return
+        exit()
 
     i, j = cell//9, cell%9
     if arr[i][j] == 0:
-        # 順に代入してみる
+        # 順に代入する
         for n in range(1, 10):
 
             # --- 行 ---
@@ -39,9 +39,12 @@ def solve(arr, cell):
         solve(arr, cell+1)
 
 
+
 def main():
     arr = [list(map(int, input().split())) for _ in range(9)]
+
     solve(arr, 0)
+
 
 if __name__ == "__main__":
     main()
