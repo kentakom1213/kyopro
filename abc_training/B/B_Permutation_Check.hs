79e347902d6860aa416bf30c5d892c932afb39e3
kentakom1213 <kentakom1213@gmail.com>
2022-05-14 00:48:12 +0900

    haskell 復習

---
+++ abc_training/B/B_Permutation_Check.hs
@@ -0,0 +1,14 @@
+
+--          B - Permutation Check          
+-- ----------------------------------------
+-- 問題
+-- https://atcoder.jp/contests/abc205/tasks/abc205_b
+-- ----------------------------------------
+
+import Data.List
+
+main = do
+  _ <- getLine
+  a_s <- map (\x -> read x :: Int) . words <$> getLine
+
+  putStrLn $ if and [a == i | (a, i) <- zip (sort a_s) [1..]] then "Yes" else "No"
