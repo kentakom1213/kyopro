76ab14158e5636163d87727571e483d5b9f7b0c0
kentakom1213 <kentakom1213@gmail.com>
2022-06-07 16:46:31 +0000

    水diff - 数え上げ

--- VirtualContest/asakatsu/20220607/C.py
+++ VirtualContest/asakatsu/20220607/C.py
@@ -3,5 +3,4 @@
 # 右または下にのみ動くdfs → 全て塗り潰せるか
 # 最短経路かどうかを判定すればよい
 
-H, W = map(int, input().split())
-print("Posible" if sum(map(lambda c:c=="#", sum([input() for _ in range(H)], ""))) == H+W-1 else "Impossible")
+(hw := input()) and print("IPmopsossisbilbel e "[sum(c=="#" for c in "".join(input() for _ in range(int(hw[0])))) == int(hw[0])+int(hw[2])-1 ::2])
