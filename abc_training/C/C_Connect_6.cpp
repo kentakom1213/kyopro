48cdc892b7b96373016010273a91d9347053b091
kentakom1213 <kentakom1213@gmail.com>
2022-09-02 11:13:01 +0900

    [add] imosを使うのは頭がいい

--- abc_training/C/C_Connect_6.cpp
+++ abc_training/C/C_Connect_6.cpp
@@ -18,8 +18,38 @@ constexpr int mod = 998244353;
 int main() {
     int N;
     cin >> N;
-    string S[N];
+    vector<string> S(N);
     rep(i, 0, N) cin >> S[i];
 
-    
+    bool isOK = false;
+
+    // 横, 縦
+    rep(i, 0, N) {
+        rep(j, 0, N-6+1) {
+            int h=0, v=0;
+            rep(k, 0, 6) {
+                h += (S[i][j+k] == '.');  // 横
+                v += (S[j+k][i] == '.');  // 縦
+            }
+            isOK |= (h <= 2 || v <= 2);
+        }
+    }
+
+    // 斜め
+    rep(i, 0, N) {
+        rep(j, 0, N) {
+            int lu=6, ru=6;  // 左上, 右上
+            rep(k, 0, 6) {
+                if (i+6 <= N && j+6 <= N) {
+                    lu -= (S[i+k][j+k] == '#');
+                }
+                if (i-5 >= 0 && j+6 <= N) {
+                    ru -= (S[i-k][j+k] == '#');
+                }
+            }
+            isOK |= (lu <= 2 || ru <= 2);
+        }
+    }
+
+    cout << (isOK ? "Yes" : "No") << endl;
 }
