c67c909076282b609d81c360cb0a249b100cd981
kentakom1213 <kentakom1213@gmail.com>
2022-10-01 01:12:42 +0000

    [add] あさかつ　むずかしい

---
+++ abc_training/D/D_Rain_Flows_into_Dams.cpp
@@ -0,0 +1,22 @@
+//         D - Rain Flows into Dams        
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc133/tasks/abc133_d
+// ----------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(A) A.begin(), A.end()
+using ll = long long;
+template <typename T> inline bool chmax(T &a, const T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> inline bool chmin(T &a, const T b) { if (a > b) { a = b; return true; } return false; }
+using pll = pair<ll, ll>;
+constexpr int MOD = 1000000007;
+constexpr int mod = 998244353;
+
+int main() {
+    int N; cin >> N;
+    vector<int> A(N);
+    for (int i=0; i<N; i++) cin >> A[i];
+    
+}
