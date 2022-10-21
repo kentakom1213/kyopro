a91fd15ae2f8c1fd522c20aa21c851a9cfe5f905
kentakom1213 <kentakom1213@gmail.com>
2022-10-21 22:38:37 +0900

    [add] 永遠にWA

--- abc_training/E/E_Erasing_Vertices_2.rs
+++ abc_training/E/E_Erasing_Vertices_2.rs
@@ -65,11 +65,55 @@ fn main() {
     let A = get!(usize;;);
     let mut G = vec![vec![]; N];
     for _ in 0..M {
-        let (u, v) = get!(usize, usize);
-        G[u-1].push(v-1);
-        G[v-1].push(u-1);
+        let (mut u, mut v) = get!(usize, usize);
+        u -= 1; v -= 1;
+        G[u].push(v);
+        G[v].push(u);
     }
 
-    
+    let can = |m: usize| -> bool {
+        let mut cost = vec![0; N];
+        for u in 0..N {
+            for &v in &G[u] {
+                cost[u] += A[v];
+            }
+        }
+        let mut is_delete = vec![false; N];
+        let mut que = VecDeque::new();
+        for i in 0..N {
+            if cost[i] <= m {
+                que.push_back(i);
+            }
+        }
+        while !que.is_empty() {
+            let u = que.pop_front().unwrap();
+            is_delete[u] = true;
+            for &v in &G[u] {
+                if !is_delete[v] {
+                    cost[v] = if cost[v] <= A[u] { 0 } else { cost[v] - A[u] };
+                    if cost[v] <= m {
+                        que.push_back(v);
+                    }
+                }
+            }
+        }
+
+        is_delete
+            .iter()
+            .fold(true, |acc, x| acc & x)
+    };
+
+    let mut left = 0;
+    let mut right = 1001001001001;
+    while (right - left) > 1 {
+        let mid = (left + right) / 2;
+        if can(mid) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+
+    println!("{}", right);
 }
 
