ce73c93ce53f25556eade7643b33152f512e6aba
kentakom1213 <kentakom1213@gmail.com>
2022-10-23 17:28:31 +0900

    [add] 期待値

---
+++ abc_training/D/D_Robot_Arms_2.rs
@@ -0,0 +1,85 @@
+//             D - Robot Arms 2            
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc274/tasks/abc274_d
+// ----------------------------------------
+
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
+// imports
+use std::collections::{HashMap, BTreeMap, VecDeque, BinaryHeap};
+use std::cmp::Reverse;
+
+// input macro
+// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+
+
+// solve
+fn main() {
+    let (N, x, y) = get!(usize, isize, isize);
+    let A = get!(isize;;);
+
+    const SIZE: usize = 40000;
+    const OFFSET: isize = 20000;
+
+    let dpx = vec![vec![false; SIZE]; N/2];
+    let dpy = vec![vec![false; SIZE]; N/2];
+
+    // 初期化
+    dpx[0][OFFSET + x] = true;
+    dpy[0][OFFSET] = true;
+
+    // 更新
+    for i in 1..N {
+        if i % 2 == 0 {
+
+        }
+    }
+}
+
---
+++ abc_training/E/E_Throwing_the_Die.rs
@@ -0,0 +1,78 @@
+//           E - Throwing the Die          
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc266/tasks/abc266_e
+// ----------------------------------------
+
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
+// imports
+use std::collections::{HashMap, BTreeMap, VecDeque, BinaryHeap};
+use std::cmp::Reverse;
+
+// input macro
+// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+
+
+// solve
+fn main() {
+    let N = get!(usize);
+
+    let mut f = vec![0.0; 101];
+    f[1] = 3.5;
+
+    for n in 2..=100 {
+        for i in 1..=6 {
+            f[n] += (i as f64).max(f[n-1]) / 6.0;
+        }
+    }
+
+    println!("{}", f[N]);
+}
+
