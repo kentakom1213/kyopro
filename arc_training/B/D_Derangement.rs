d3e7e657bf154fbe80c79bf510d6306311ca9792
kentakom1213 <kentakom1213@gmail.com>
2022-10-29 00:49:14 +0900

    [add] 型変換難しい

---
+++ arc_training/B/D_Derangement.rs
@@ -0,0 +1,88 @@
+//             D - Derangement             
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/abc072/tasks/arc082_b
+// ----------------------------------------
+
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+// imports
+use std::collections::{HashMap, BTreeMap, VecDeque, BinaryHeap};
+use std::cmp::Reverse;
+
+// input macro
+// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+
+// static vales
+static MOD1: usize = 1_000_000_007;
+static MOD9: usize = 998_244_353;
+
+
+// solve
+fn main() {
+    let N = get!(usize);
+    let A = get!(usize;;);
+
+    let mut cnt = 0;
+    let mut swapped = false;
+    for (i, &a) in A[..N-1].iter().enumerate() {
+        if i == a {
+            if swapped {
+                continue;
+            } else {
+                cnt += 1;
+                swapped = true;
+            }
+        }
+    }
+
+    println!("{}", cnt);
+}
+
