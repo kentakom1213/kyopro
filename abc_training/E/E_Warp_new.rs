57985ef55dacece8916a5097e53f090f327d442b
kentakom1213 <kentakom1213@gmail.com>
2022-11-01 00:41:37 +0900

    :fire: Rustむきではない問題

--- abc_training/E/E_Warp_new.rs
+++ abc_training/E/E_Warp_new.rs
@@ -6,9 +6,7 @@
 
 /*
 # 解説
-dp[n][x][y] := n回の移動後に(x, y)にいるような移動経路の個数
-
-↑こっちは思いつきそう
+dp[n][x][y] := 
 */
 
 // attributes
@@ -18,7 +16,7 @@ dp[n][x][y] := n回の移動後に(x, y)にいるような移動経路の個数
 #![allow(dead_code)]
 
 // imports
-use std::collections::{BTreeMap, VecDeque, BinaryHeap, BTreeSet};
+use std::collections::{HashSet, BTreeMap, VecDeque, BinaryHeap};
 use std::cmp::Reverse;
 
 // input macro
@@ -71,24 +69,32 @@ macro_rules! get {
 static MOD1: usize = 1_000_000_007;
 static MOD9: usize = 998_244_353;
 
+
 // solve
 fn main() {
     let (N, M) = get!(usize, usize);
     let A = get!(isize;;);
     let mov = [(A[0], A[1]), (A[2], A[3]), (A[4], A[5])];
-    let obs: BTreeSet<(isize, isize)> = get!(isize, isize; M).into_iter().collect();
+    let obs: HashSet<(isize, isize)> = get!(isize, isize; M).into_iter().collect();
 
-    let mut dp = BTreeMap::new();
-    dp.insert((0, 0), 1);
-
+    let mut dp = vec![vec![1]];
     for i in 0..N {
-        let mut new_dp = BTreeMap::new();
-        for (&(x, y), &val) in &dp {
-            for &(dx, dy) in &mov {
-                let new_pos = (x+dx, y+dy);
-                if !obs.contains(&new_pos) {
-                    let &old_val = new_dp.get(&new_pos).unwrap_or(&0);
-                    new_dp.insert(new_pos, (old_val + val)%MOD9);
+        let mut new_dp = vec![vec![0; i*2]; i*2];
+        for a in 0..=i {
+            for b in 0..=i {
+                let i = i as isize;
+                let a = a as isize;
+                let b = b as isize;
+
+                let x = a*mov[0].0 + b*mov[1].0 + (i-a-b)*mov[2].0;
+                let y = a*mov[0].1 + b*mov[1].1 + (i-a-b)*mov[2].1;
+
+                for k in 0..3 {
+                    let (dx, dy) = mov[k];
+                    if obs.contains(&(x+dx, y+dy)) {
+                        new_dp[a as usize + (if k==0 {1} else {0})][b as usize + (if k==1 {1} else {0})] += dp[a as usize][b as usize];
+                        new_dp[a as usize + (if k==0 {1} else {0})][b as usize + (if k==1 {1} else {0})] %= MOD9;
+                    }
                 }
             }
         }
@@ -96,9 +102,11 @@ fn main() {
     }
 
     let mut ans = 0;
-    for (_, &val) in &dp {
-        ans = (ans + val) % MOD9;
+    for row in &dp {
+        for &v in row {
+            ans = (ans + v) % MOD9;
+        }
     }
-
     println!("{}", ans);
 }
+
