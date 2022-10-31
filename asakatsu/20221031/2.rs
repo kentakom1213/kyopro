dffeaee100b2d6a93e6926fda8fa74aa159eeda8
kentakom1213 <kentakom1213@gmail.com>
2022-10-31 09:05:23 +0900

    :sunny: あさかつ20221031

---
+++ asakatsu/20221031/2.rs
@@ -0,0 +1,77 @@
+// https://atcoder.jp/contests/agc002/tasks/agc002_a
+
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+// imports
+use std::collections::{HashMap, BTreeMap, VecDeque, BinaryHeap};
+use std::cmp::Reverse;
+
+// input macro
+// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+
+// static vales
+static MOD1: usize = 1_000_000_007;
+static MOD9: usize = 998_244_353;
+
+// solve
+fn main() {
+    let (a, b) = get!(isize, isize);
+
+    if a <= 0 && 0 <= b {
+        println!("Zero");
+    }
+    else if a > 0 || (b - a) % 2 == 1 {
+        println!("Positive");
+    }
+    else {
+        println!("Negative");
+    }
+}
+
