5aaa10583c80824d1705e4728c470b650a20edf1
kentakom1213 <kentakom1213@gmail.com>
2022-11-06 11:45:56 +0900

    :clock3: 定数倍高速化テク

--- abc_training/E/E_Round_Trip.py
+++ abc_training/E/E_Round_Trip.py
@@ -4,46 +4,97 @@
 # https://atcoder.jp/contests/abc276/tasks/abc276_e
 # ----------------------------------------
 
-import sys
-sys.setrecursionlimit(1000000)
-from itertools import combinations
+from collections import defaultdict
 
-def yes():
-    print("Yes")
-    exit()
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
 
-def no():
-    print("No")
-    exit()
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
 
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-if __name__ == "__main__":
+        if x == y:
+            return
 
-    H, W = map(int, input().split())
-    field = [input() for _ in range(H)]
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
 
-    sr = sc = 0
-    for i in range(H):
-        for j in range(W):
-            if field[i][j] == "S":
-                sr, sc = i, j
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
 
-    # `S`に隣接するマスを生成
-    adj_cells = []
+    def size(self, x):
+        return -self.parents[self.find(x)]
 
-    for i, j in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        nr, nc = sr+i, sc+j
-        if i==j==0 \
-        or not 0 <= nr < H \
-        or not 0 <= nc < W \
-        or field[nr][nc] == "#":
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+# solve
+H, W = map(int, input().split())
+C = [input() for _ in range(H)]
+
+sr, sc = 0, 0
+for i in range(H):
+    for j in range(W):
+        if C[i][j] == "S":
+            sr, sc = i, j
+
+# 連結判定
+uf = UnionFind(H*W)
+for i in range(H):
+    for j in range(W):
+        v = W*i+j
+        if C[i][j] != ".":
             continue
+        if i+1 < H and C[i+1][j] == ".":
+            nv = W*(i+1)+j
+            uf.union(v, nv)
+        if j+1 < W and C[i][j+1] == ".":
+            nv = W*i+j+1
+            uf.union(v, nv)
 
-        adj_cells.append((nr, nc))
+adj = []
+for dr, dc in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+    nr, nc = sr+dr, sc+dc
+    if 0 <= nr < H \
+    and 0 <= nc < W \
+    and C[nr][nc] == ".":
+        adj.append((nr, nc))
 
-    # 隣接するセルの組合せについて全探索
-    for a, b in combinations(adj_cells, 2):
-        
-    
-    no()
-    
+ladj = len(adj)
+for i in range(ladj):
+    for j in range(i+1, ladj):
+        a, b = adj[i], adj[j]
+
+        if uf.same(W*a[0]+a[1], W*b[0]+b[1]):
+            print("Yes")
+            exit()
+
+print("No")
---
+++ arc_training/B/pass/B_Village_of_M_People.rs
@@ -0,0 +1,93 @@
+//         B - Village of M People         
+// ----------------------------------------
+// 問題
+// https://atcoder.jp/contests/arc118/tasks/arc118_b
+// ----------------------------------------
+
+/*
+
+# 方針
+- 二分探索
+
+    max|Bi/M - Ai/N|
+<=> max|N*Bi - M*Ai|
+
+## 参考
+- https://atcoder.jp/contests/arc118/submissions/22249436
+
+*/
+
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+// imports
+use std::collections::{HashMap, BTreeMap, VecDeque, BinaryHeap};
+use std::cmp::Reverse;
+
+// input macro
+// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+
+// static vales
+static MOD1: usize = 1_000_000_007;
+static MOD9: usize = 998_244_353;
+static INF: usize = 1001001001001001001;
+
+
+// solve
+fn main() {
+    let (K, N, M) = get!(usize, usize, usize);
+    let A = get!(usize;;);
+
+    let check = |x: usize| {
+        let L = 0.max(
+            
+        );
+    };
+}
+
