bf16edc422d1c4f707cae8e753754bbd33fc4a75
kentakom1213 <kentakom1213@gmail.com>
2022-11-19 09:12:10 +0900

    :sunny: 難しい

---
+++ asakatsu/20221119/E_Destruction.rs
@@ -0,0 +1,146 @@
+// https://atcoder.jp/contests/abc218/tasks/abc218_e
+
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+// imports
+use std::cmp::{max, min, Reverse};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+// input macro
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    // ($($t:ty);*) => {
+    //     (
+    //         $(get!($t),)*
+    //     )
+    // };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+
+// [RustでUnionFind](https://zenn.dev/nakamurus/articles/f398b7f4d7618ea5b7eb)
+struct UnionFind {
+    par: Vec<usize>,
+    siz: Vec<usize>,
+}
+
+impl UnionFind {
+    // UnionFindを新規作成
+    fn new(n: usize) -> Self {
+        UnionFind {
+            par: (0..n).collect(),
+            siz: vec![1; n],
+        }
+    }
+
+    // 根を求める
+    fn root(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;
+        }
+        self.par[x] = self.root(self.par[x]);  // 経路圧縮
+        self.par[x]
+    }
+
+    // 同一の集合に所属するか判定
+    fn issame(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    // 要素を結合
+    fn unite(&mut self, mut parent: usize, mut child: usize) -> bool {
+        parent = self.root(parent);
+        child = self.root(child);
+
+        if parent == child {
+            return false;
+        }
+
+        // 要素数が大きい方を子にすることで、高さを均等に保つ
+        if self.siz[parent] < self.siz[child] {
+            std::mem::swap(&mut parent, &mut child);
+        }
+
+        self.par[child] = parent;
+        self.siz[parent] += self.siz[child];
+        true
+    }
+
+    fn size(&mut self, x: usize) -> usize {
+        let root = self.root(x);
+        self.siz[root]
+    }
+
+}
+
+// static vales
+const MOD1: usize = 1_000_000_007;
+const MOD9: usize = 998_244_353;
+const INF: usize = 1001001001001001001;
+
+// solve
+fn main() {
+    let (N, M) = get!(usize, usize);
+    let mut edges = vec![];
+    for _ in 0..M {
+        let (a, b, c) = get!(usize, usize, isize);
+        edges.push( (c, a-1, b-1) );
+    }
+
+    edges.sort();
+
+    // クラスカル法
+    let mut ans = 0;
+    let mut uf = UnionFind::new(N);
+    for (c, a, b) in edges {
+        if c <= 0 || !uf.issame(a, b) {
+            uf.unite(a, b);
+        } else {
+            ans += c;
+        }
+    }
+
+    println!("{}", ans);
+}
