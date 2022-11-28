e38a1dabf2dce2af53bc5cc47de4bb179198331a
kentakom1213 <kentakom1213@gmail.com>
2022-11-28 21:42:42 +0900

    [add] 典型アルゴリズム

---
+++ Others/typical_algorithm/E_全点対最短経路問題.rs
@@ -0,0 +1,96 @@
+// https://atcoder.jp/contests/typical-algorithm/tasks/typical_algorithm_e
+
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+// imports
+use std::cmp::{max, min, Reverse};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+// input macro
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    // ($($t:ty);*) => {
+    //     (
+    //         $(get!($t),)*
+    //     )
+    // };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+
+// constant
+const MOD1: usize = 1_000_000_007;
+const MOD9: usize = 998_244_353;
+const INF: usize = 1001001001001001001;
+
+// solve
+fn main() {
+    let (N, M) = get!(usize, usize);
+    let mut G = vec![vec![INF; N]; N];
+    for _ in 0..M {
+        let (a, b, c) = get!(usize, usize, usize);
+        G[a][b] = c;
+    }
+
+    // ワーシャル・フロイド法
+    for p in 0..N {
+        for u in 0..N {
+            for v in 0..N {
+                G[u][v] = G[u][v].min(
+                    G[u][p] + G[p][v]
+                );
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..N {
+        for j in 0..N {
+            ans += if i == j { 0 } else { G[i][j] };
+        }
+    }
+
+    println!("{}", ans);
+}
