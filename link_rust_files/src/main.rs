9ae7fe081eeb1464d627843df5a5e12dd27d5e23
kentakom1213 <kentakom1213@gmail.com>
2022-12-06 21:04:37 +0900

    [rename] 名前を変更

--- link_rust_files/src/main.rs
+++ link_rust_files/src/main.rs
@@ -1,18 +1,22 @@
-// 参考：https://makandat.wordpress.com/2022/02/08/rust-%E3%81%AE%E5%8B%89%E5%BC%B7-%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E4%B8%80%E8%A6%A7/
+// # 参考
+// - https://makandat.wordpress.com/2022/02/08/rust-%E3%81%AE%E5%8B%89%E5%BC%B7-%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E4%B8%80%E8%A6%A7/
+// - https://qiita.com/benki/items/70ad2ee44cff9efde778
 use std::env;
 use std::io;
 use std::fs;
 use std::vec;
 use std::path;
+use colored::*;
  
 /* ファイル一覧を取得する。 (ディレクトリを除く) */
 fn get_files(target_dir: &str) -> io::Result<vec::Vec<path::PathBuf>> {
     // 対象のファイル一覧(io::Result<ReadDir>)を取得する。ReadDir は DirEntry を返す反復子。
-    let mut entries = fs::read_dir(target_dir)?
-       .map(|res| res.map(|e| e.path()))
-       .collect::<Result<Vec<_>, io::Error>>()?;
-    entries.sort();
-    return Ok(entries);
+    Ok(fs::read_dir(target_dir)?
+        .map(|res|
+            res.map(|e| e.path())
+        )
+        .collect::<Result<Vec<_>, io::Error>>()?
+    )
 }
 
 fn crawl_dir(root_dir: &str) {
@@ -23,20 +27,19 @@ fn crawl_dir(root_dir: &str) {
                 let s = x.as_path().to_str().unwrap();
                 if path::Path::new(s).is_file() {
                     if s.ends_with(".rs") {
-                        println!("{}", s);
+                        println!("{}", s.blue());
                     }
                 } else {
                     crawl_dir(s);  // 再帰的に検索
                 }
             }
         },
-        Err(s) => println!("{}", s),
+        Err(_) => (),
     }
 }
  
 /* メインプログラム */
 fn main() {
-    println!("** ファイル一覧 (ディレクトリを除く) **");
     let argv: Vec<String> = env::args().collect();
     let mut target_dir: &str = ".";  // 対象のディレクトリ
     // let mut input = String::new();  // 作業用
