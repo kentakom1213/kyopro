218057b95691d62bafb8a96816c6ef2b38dd2fc4
kentakom1213 <kentakom1213@gmail.com>
2023-01-23 02:20:06 +0900

    [add] ノードの削除

--- learn_algorithm/LinkedList/src/main.rs
+++ learn_algorithm/LinkedList/src/main.rs
@@ -7,6 +7,12 @@ macro_rules! linked_list {
             next: Box::new( linked_list!( $( $vals, )* ) ),
         }
     }};
+    ( $val:expr $(,)* ) => {{
+        LinkedList::Node {
+            val: $val,
+            next: Box::new( linked_list!() ),
+        }
+    }};
     () => {{
         LinkedList::Null   
     }};
@@ -27,12 +33,55 @@ impl<T> LinkedList<T>
 where
     T: std::cmp::Eq
 {
+    /// ## replace
+    /// `next`を引数で置き換える。
+    fn replace(&mut self, node: Self) {
+        *self = node;
+    }
 
+    /// ## remove
+    /// ノードを削除する。
+    fn remove(&mut self) {
+        *self = LinkedList::Null;
+    }
+
+    // fn insert(&mut self, x: T) {
+    //     // 新しく追加するノード
+    //     let mut new_node = Box::new(
+    //         LinkedList::Node { val: x, next: Box::new(LinkedList::Null) }
+    //     );
+
+    // }
 }
 
 
 fn main() {
-    let list2 = linked_list!(1, 2, 3, 4);
+    let mut list = linked_list!(1, 2, 3, 4);
+    println!("{:?}", list);
 
-    println!("{:?}", list2);
+    if let LinkedList::Node{ val, next } = &mut list {
+        println!("置き換え前");
+        println!("val: {}", val);
+        println!("next: {:?}", next);
+
+        // 置き換える
+        let new_node = linked_list!(100);
+        next.replace(new_node);
+
+        println!("置き換え後");
+        println!("val: {}", val);
+        println!("next: {:?}", next);
+    }
+
+    println!("{:?}", list);
+
+    if let LinkedList::Node{ val:_, next } = &mut list {
+        next.remove();
+    }
+
+    println!("{:?}", list);
+
+    list.remove();
+
+    println!("{:?}", list);
 }
