54377ba9785fda240dd21a911f7971e052c68958
kentakom1213 <kentakom1213@gmail.com>
2023-01-23 11:30:58 +0900

    [add] 探索の実装

--- learn_algorithm/LinkedList/src/main.rs
+++ learn_algorithm/LinkedList/src/main.rs
@@ -51,19 +51,44 @@ where
         match self {
             LinkedList::Null => None,
             LinkedList::Node { val: _, next } => {
-                match **next {
-                    LinkedList::Null => None,
-                    _ => Some( &mut **next )
-                }
+                Some( &mut **next )
             },
         }
     }
 
-    // /// ## nth
-    // /// 与えられたノードから数えてn番目のノードを返す
-    // fn nth(&mut self, n: usize) -> &mut Option<LinkedList<T>> {
+    /// ## nth
+    /// 与えられたノードから数えてn番目のノードを返す
+    fn nth(&mut self, n: usize) -> Option<&mut LinkedList<T>> {
+        let mut res = self;
 
-    // }
+        for _ in 0..n {
+            match res {
+                LinkedList::Null => {
+                    return None;
+                },
+                LinkedList::Node { val: _, next } => {
+                    res = &mut **next;
+                },
+            }
+        }
+
+        Some( res )
+    }
+
+    /// ## find
+    /// 与えられたノードの子要素から、要素`x`を探索する
+    fn find(&self, x: T) -> Option<&LinkedList<T>> {
+        let mut res = self;
+
+        while let LinkedList::Node { val, next } = res {
+            if *val == x {
+                return Some( res )
+            }
+            res = &**next;
+        }
+
+        None
+    }
 
     // fn insert(&mut self, x: T) {
     //     // 新しく追加するノード
@@ -76,16 +101,22 @@ where
 
 
 fn main() {
+    // --- アクセス ---
+    println!("--- アクセス ---");
+
     let mut list = linked_list!(1, 2, 3, 4);
     println!("{:?}", list);
 
-    let third_elem = list.get_next().unwrap().get_next().unwrap();
+    let third_elem = list.nth(4).unwrap();
     println!("{:?}", third_elem);
 
     third_elem.remove();
     println!("{:?}", third_elem);
 
     println!("{:?}", list);
+
+    // --- 要素の置き換え ---
+    println!("\n--- 置換 ---");
 
     if let LinkedList::Node{ val, next } = &mut list {
         println!("置き換え前");
@@ -113,5 +144,17 @@ fn main() {
 
     println!("{:?}", list);
 
+    // --- 探索 ---
+    println!("\n--- 探索 ---");
+    let list = linked_list!(1, 6, 4, 7, 8, 3, 10, 2);
+    println!("list: {:?}", list);
 
+    let find_3 = list.find(3);
+    println!("list.find(3) = {:?}", find_3);
+
+    let find_2 = list.find(2);
+    println!("list.find(2) = {:?}", find_2);
+
+    let find_100 = list.find(100);
+    println!("list.find(100) = {:?}", find_100);
 }
