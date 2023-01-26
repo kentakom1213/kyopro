22628ff8721b5abe78fb91a755911b457fdd0e38
kentakom1213 <kentakom1213@gmail.com>
2023-01-26 16:36:36 +0900

    [add] F問題

--- abc_training/F/F_ABCBAC.rs
+++ abc_training/F/F_ABCBAC.rs
@@ -17,14 +17,34 @@ use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDequ
 use proconio::{input, fastout, marker::{Chars, Bytes, Usize1}};
 
 // constant
-const MOD1: usize = 1_000_000_007;
-const MOD9: usize = 998_244_353;
-const INF: usize = 1001001001001001001;
-const NEG1: usize = 1_usize.wrapping_neg();
+const MOD: usize = 998244353;
+
+/// # Modint
+trait Modint {
+    fn madd(&self, other: usize) -> usize;
+    fn msub(&self, other: usize) -> usize;
+    fn mmul(&self, other: usize) -> usize;
+}
+
+impl Modint for usize {
+    fn madd(&self, other: usize) -> usize {
+        (*self + other) % MOD
+    }
+    fn msub(&self, other: usize) -> usize {
+        (MOD + *self - other) % MOD
+    }
+    fn mmul(&self, other: usize) -> usize {
+        let res: u128 = (*self as u128) * (other as u128);
+        (res % MOD as u128) as usize
+    }
+}
 
 /// # RollingHash
+/// 文字列の比較を高速に行う
+/// - 計算量: `O(n+m)`
 #[derive(Debug)]
 struct RollingHash {
+    size: usize,
     power: Vec<usize>,
     hash: Vec<usize>,
     base: usize,
@@ -40,20 +60,15 @@ impl RollingHash {
         let mut hash = vec![0; size + 1];
 
         // hashを初期化
-        let mut v = 0;
+        let (mut h, mut p) = (0, 1);
         for i in 0..size {
-            v = Self::madd( Self::mmul(v, base), arr[i]);
-            hash[i+1] = v;
+            h = arr[i].madd(h.mmul(base));
+            p = p.mmul(base);
+            hash[i+1] = h;
+            power[i+1] = p;
         }
 
-        // powerを初期化
-        let mut v = 1;
-        for i in 0..size {
-            v = Self::mmul(v, base);
-            power[i+1] = v;
-        }
-
-        Self { power, hash, base }
+        Self { size, power, hash, base }
     }
 
     /// 文字列から生成
@@ -67,11 +82,23 @@ impl RollingHash {
     }
 
     /// `l..r`のハッシュを取得
+    /// - 計算量: `O(1)`
     fn get(&self, l: usize, r: usize) -> usize {
-        Self::msub(
-            self.hash[r],
-            Self::mmul(self.hash[l], self.power[r-l])
+        self.hash[r].msub(
+            self.hash[l].mmul(self.power[r-l])
         )
+    }
+
+    /// `0..size`のハッシュを取得
+    /// - 計算量: `O(1)`
+    fn full(&self) -> usize {
+        self.hash[self.size]
+    }
+
+    /// ハッシュ同士を連結
+    /// - 計算量: `O(1)`
+    fn connect(&self, h1: usize, h2:usize, h2_len: usize) -> usize {
+        h1.mmul(self.power[h2_len]).madd(h2)
     }
 
     /// `A`を`0`とするascii文字(`A~Za~z`)のインデックスを返す
@@ -79,27 +106,6 @@ impl RollingHash {
         let a = 'A' as u32;
         let c = c as u32;
         (c - a) as usize
-    }
-
-    /// 足し算
-    fn madd(mut a: usize, b: usize) -> usize {
-        a += b;
-        if a >= Self::MOD { a -= Self::MOD; }
-        a
-    }
-
-    /// 引き算
-    fn msub(mut a: usize, b: usize) -> usize {
-        a += Self::MOD;
-        a -= b;
-        while a >= Self::MOD { a -= Self::MOD }
-        a
-    }
-
-    /// 掛け算
-    fn mmul(a: usize, b: usize) -> usize {
-        let c: u128 = (a as u128) * (b as u128);
-        (c % Self::MOD as u128) as usize
     }
 }
 
@@ -110,8 +116,19 @@ fn main() {
         S: String,
     }
 
-    let base = 20021213;
-    let roliha = RollingHash::from_str(&S, base);
+    let base = 27;
+    let th = RollingHash::from_str(&S, base);
+    let rth = RollingHash::from_str(&(S.chars().rev().collect::<String>()), base);
 
-    println!("{:#?}", roliha);
+    for i in 0..=N {
+        let x = th.get(0, i);
+        let y = th.get(N+i, 2*N);
+        if th.connect(x, y, N-i) == rth.get(N-i, 2*N-i) {
+            println!("{}", S[..i].to_string() + &S[i+N..]);
+            println!("{}", i);
+            return;
+        }
+    }
+
+    println!("-1");
 }
