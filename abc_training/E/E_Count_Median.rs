f2ab68b0131bd89781f6acaa8d20a4f6df7578f8
kentakom1213 <kentakom1213@gmail.com>
2023-01-30 12:31:08 +0900

    [add] 解説AC

--- abc_training/E/E_Count_Median.rs
+++ abc_training/E/E_Count_Median.rs
@@ -1,4 +1,4 @@
-//             E - Count Median            
+//             E - Count Median
 // ----------------------------------------
 // 問題
 // https://atcoder.jp/contests/abc169/tasks/abc169_e
@@ -6,57 +6,37 @@
 
 #![allow(non_snake_case)]
 
-// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
-macro_rules! get {
-    ($t:ty) => {
-        {
-            let mut line = String::new();
-            std::io::stdin().read_line(&mut line).unwrap();
-            line.trim().parse::<$t>().unwrap()
-        }
-    };
-    ($($t:ty),*) => {
-        {
-            let mut line = String::new();
-            std::io::stdin().read_line(&mut line).unwrap();
-            let mut iter = line.split_whitespace();
-            (
-                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
-            )
-        }
-    };
-    ($t:ty ; $n:expr) => {
-        (0..$n).map(|_|
-            get!($t)
-        ).collect::<Vec<_>>()
-    };
-    ($($t:ty),* ; $n:expr) => {
-        (0..$n).map(|_|
-            get!($($t),*)
-        ).collect::<Vec<_>>()
-    };
-    ($t:ty ;;) => {
-        {
-            let mut line = String::new();
-            std::io::stdin().read_line(&mut line).unwrap();
-            line.split_whitespace()
-                .map(|t| t.parse::<$t>().unwrap())
-                .collect::<Vec<_>>()
-        }
-    };
-    ($t:ty ;; $n:expr) => {
-        (0..$n).map(|_|
-            get!($t ;;)
-        ).collect::<Vec<_>>()
-    };
-}
-
+use proconio::input;
 
 /// ## 方針
+/// 中央値の最小値、中央値の最大値の間の値を考える
+/// Nが奇数のときは1刻み、Nが偶数のときは1/2刻みで
+/// 中央値が現れる。
 fn main() {
-    let N = get!(usize);
-    let AB = get!(usize, usize; N);
+    input! {
+        N: usize,
+        AB: [(usize, usize); N],
+    }
 
-    // 
+    let (mut A, mut B) = (vec![], vec![]);
+    for &(a, b) in &AB {
+        A.push(a);
+        B.push(b);
+    }
+
+    A.sort();
+    B.sort();
+
+    // A, Bそれぞれの中央値を考える
+    if N % 2 == 0 {
+        let med_a = (A[N / 2 - 1] + A[N / 2]);
+        let med_b = (B[N / 2 - 1] + B[N / 2]);
+        let ans = med_b - med_a + 1;
+        println!("{}", ans);
+    } else {
+        let med_a = A[N / 2];
+        let med_b = B[N / 2];
+        let ans = med_b - med_a + 1;
+        println!("{}", ans);
+    }
 }
-
