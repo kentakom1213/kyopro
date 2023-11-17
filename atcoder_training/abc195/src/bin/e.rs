af62118d4e72d001a0c117b9a2b7c317a9d61824
kentakom1213 <kentakom1213@gmail.com>
2023-11-17 18:28:39 +0900

    [add] 青diff

--- atcoder_training/abc195/src/bin/e.rs
+++ atcoder_training/abc195/src/bin/e.rs
@@ -28,12 +28,35 @@ fn main() {
         X: String,
     }
 
-    let S = S
-        .chars()
-        .map(|c| c.to_digit(10).unwrap() as usize)
-        .collect_vec();
+    // dp[i][r] :=
+    //     'A' => {r | (10*r%7) in dp[i+1] or ((10*r + S[i])%7) in dp[i]}
+    //     'T' => {r | (10*r%7) in dp[i+1] and ((10*r + S[i])%7) in dp[i]}
+    let mut dp = vec![0_u8; N + 1];
+    dp[N] = 1;
 
-    // dp[i] := 
-    //     X[i] == 'A' => 
-    //     X[i] == 'T' => 
+    // 後ろから埋めていく
+    for (i, (d, x)) in (0..N).rev().zip(S.chars().rev().zip(X.chars().rev())) {
+        let d = d.to_digit(10).unwrap();
+        // ビットごとに考える
+        for r in 0..7 {
+            if x == 'A' {
+                dp[i] |= (dp[i + 1] >> (10 * r % 7) & 1 & dp[i + 1] >> ((10 * r + d) % 7) & 1) << r;
+            }
+            if x == 'T' {
+                dp[i] |= (dp[i + 1] >> (10 * r % 7) & 1 | dp[i + 1] >> ((10 * r + d) % 7) & 1) << r;
+            }
+        }
+    }
+
+    if cfg!(debug_assertions) {
+        for &x in &dp {
+            eprintln!("{:0>7b}", x)
+        }
+    }
+
+    if dp[0] & 1 == 1 {
+        println!("Takahashi");
+    } else {
+        println!("Aoki");
+    }
 }
