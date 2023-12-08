d07936f5dc0e16b50608514cd9f3830ec02e7e45
kentakom1213 <kentakom1213@gmail.com>
2023-12-08 22:18:42 +0900

    [add] もすーん

---
+++ atcoder_training/abc313/src/bin/d.rs
@@ -0,0 +1,70 @@
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(unused_macros)]
+
+// imports
+use itertools::Itertools;
+use proconio::{input, marker::{Chars, Bytes, Usize1}};
+
+macro_rules! debug {
+    ( $($val:expr),* $(,)* ) => {{
+        #[cfg(debug_assertions)]
+        eprintln!( concat!($(stringify!($val), " = {:?}, "),*), $($val),* );
+    }};
+}
+
+// [Rustで競技プログラミング スターターキット](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e)
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),* ; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t ;;)
+        ).collect::<Vec<_>>()
+    };
+}
+ 
+fn main() {
+    let (N, K) = get!(usize, usize);
+
+
+}
+
+const INF: usize = 1001001001001001001;
