366ae9c203d1101f2f03d2a64f40c69265c34519
kentakom1213 <kentakom1213@gmail.com>
2023-12-13 08:31:18 +0900

    :sunny: あさかつ12/13 1位!

---
+++ virtual_contest/2653a099-59af-42eb-a591-122d3225170f/src/bin/3.rs
@@ -0,0 +1,69 @@
+// attributes
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(unused_macros)]
+
+// imports
+use itertools::Itertools;
+use proconio::{
+    input,
+    marker::{Bytes, Chars, Usize1},
+};
+
+macro_rules! debug {
+    ( $($val:expr),* $(,)* ) => {{
+        #[cfg(debug_assertions)]
+        eprintln!( concat!($(stringify!($val), " = {:?}, "),*), $($val),* );
+    }};
+}
+
+/// `chmin!{x1, x2, ..., xn}`:`x1`,`x2`,...,`xn`のうち最小のものを、`x1`に代入する
+/// - 代入があったとき、`true`を返す
+#[macro_export]
+macro_rules! chmin {
+    ( $a:expr, $b:expr $(,)* ) => {{
+        if $a > $b {
+            $a = $b;
+            true
+        } else {
+            false
+        }
+    }};
+    ( $a:expr, $b:expr, $c:expr $(,$other:expr)* $(,)* ) => {{
+        chmin! {
+            $a,
+            ($b).min($c)
+            $(,$other)*
+        }
+    }};
+}
+
+// constant
+const MOD1: usize = 1_000_000_007;
+const MOD9: usize = 998_244_353;
+const INF: usize = 1001001001001001001;
+
+fn main() {
+    input! {
+        n: usize,
+        x: usize,
+        ab: [(usize, usize); n]
+    }
+
+    // すべてのパターンをシミュレートする
+    let mut ans = INF;
+    let mut sum = 0;
+
+    for (i, &(a, b)) in ab.iter().enumerate() {
+        sum += a + b;
+        // 残り回数は、bを繰り返すことで埋める
+        chmin! {
+            ans,
+            sum + (x - i - 1) * b
+        };
+    }
+
+    println!("{ans}");
+}
