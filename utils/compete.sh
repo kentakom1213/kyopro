b159a0327add513376e4c2f5595c1bd1677e4ba9
kentakom1213 <kentakom1213@gmail.com>
2024-02-01 18:20:05 +0900

    [add] sparse table

---
+++ utils/compete.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# ディレクトリの移動
+cd ~/Docker/kyopro/atcoder_training
+
+# ファイルのコピー
+if [ -d $1 ]; then
+    if [ -f "$1/Cargo.toml" ]; then
+        echo "Directory $1 is already exists."
+        exit 0
+    else
+        mv $1 "tmp_$1"
+    fi
+fi
+
+# ディレクトリの作成
+cargo compete new $1
+
+# 戻す
+if [ -d "tmp_$1" ]; then
+    mv tmp_$1/* $1/src/bin
+    rmdir tmp_$1
+fi
---
+++ utils/snippet.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+# スニペットを作成
+SNIPPET_FILE="$KYOPURO_LIBRARY_DIR/rust.code-snippets"
+
+# ディレクトリを作成
+mkdir -p .vscode
+
+if [[ -e .vscode/rust.code-snippets ]]; then
+  # 既にファイルが存在する場合は削除
+  rm .vscode/rust.code-snippets
+fi
+
+# シンボリックリンクを作成
+ln -s $SNIPPET_FILE .vscode/rust.code-snippets
