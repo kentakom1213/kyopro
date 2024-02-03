62573a0d5cabc27c422b1f92d645f29dd1e003e6
powell <powell@pwll.tail8ffac.ts.net>
2024-02-04 01:41:27 +0900

    [add] upsolve

--- atcoder_training/abc339/src/bin/f.rs
+++ atcoder_training/abc339/src/bin/f.rs
@@ -20,10 +20,69 @@ macro_rules! debug2D {
     }};
 }
 
-use proconio::{input, marker::{Chars, Bytes, Usize1}};
+use std::{collections::HashMap, str::FromStr};
+
+use itertools::Itertools;
+use num_bigint::BigUint;
+use num_traits::ToPrimitive;
+use proconio::{
+    input,
+    marker::{Bytes, Chars, Usize1},
+};
+use rustc_hash::FxHashMap;
+
+/// 適当な素数
+const PRIMES: [usize; 2] = [938472061, 958472071];
 
 fn main() {
-    
+    input! {
+        N: usize,
+        A: [String; N],
+    }
+
+    let A = A
+        .iter()
+        .map(|x| BigUint::from_str(x).unwrap())
+        .collect_vec();
+
+    debug!(A);
+
+    let mods = A
+        .iter()
+        .map(|a| {
+            PRIMES
+                .iter()
+                .map(|&p| (a.clone() % p).to_usize().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    debug!(mods);
+
+    let mut map = HashMap::new();
+
+    for i in 0..N {
+        for j in 0..N {
+            let mul = mods[i]
+                .iter()
+                .zip(&mods[j])
+                .zip(&PRIMES)
+                .map(|((a, b), p)| a * b % p)
+                .collect_vec();
+
+            *map.entry(mul).or_insert(0) += 1;
+        }
+    }
+
+    let mut ans = 0;
+
+    for k in 0..N {
+        if let Some(v) = map.get(&mods[k]) {
+            ans += v;
+        }
+    }
+
+    println!("{ans}");
 }
 
 const INF: usize = 1001001001001001001;
