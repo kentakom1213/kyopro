08ee925a2a0d77ff051bf44773bceb71573e9176
kentakom1213 <kentakom1213@gmail.com>
2024-02-15 16:19:42 +0900

    [add] range affine queryをlibrary checkerでテスト

--- LibraryChecker/lib.rs
+++ LibraryChecker/lib.rs
@@ -1,11 +1,18 @@
-mod A_add_B;
-mod AssociativeArray {
-    mod btreemap;
-    mod hashmap;
-    mod splaytree_multiset;
+mod a_add_b {
+    mod A_add_B;
 }
-mod Predecessor_Problem {
-    mod BTreeSet;
-    mod SplayTree;
+mod associative_array {
+    mod btreemap_;
+    mod hashmap_;
+    mod splaytree_multiset_;
 }
-mod ShortestPath;
+mod predecessor_problem {
+    mod BTreeSet_;
+    mod SplayTree_;
+}
+mod range_affine_point_get {
+    mod range_affine_point_get_;
+}
+mod shortest_path {
+    mod shortest_path;
+}
