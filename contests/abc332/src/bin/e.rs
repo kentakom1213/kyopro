14b8c178227b56dfaf202836af216890719eae89
kentakom1213 <kentakom1213@gmail.com>
2024-08-03 00:08:08 +0900

    [add] 部分集合の列挙など

--- contests/abc332/src/bin/e.rs
+++ contests/abc332/src/bin/e.rs
@@ -1,29 +1,54 @@
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 #![allow(non_snake_case)]
-#![allow(dead_code)]
-#![allow(unused_macros)]
 
-macro_rules! debug {
-    ( $($val:expr),* $(,)* ) => {{
-        #[cfg(debug_assertions)]
-        eprintln!( concat!($(stringify!($val), " = {:?}, "),*), $($val),* );
-    }};
-}
-macro_rules! debug2D {
-    ( $array:expr ) => {{
-        #![cfg(debug_assertions)]
-        eprintln!("{}: ", stringify!($array));
-        for row in &$array {
-            eprintln!("{:?}", row);
-        }
-    }};
-}
+use cp_library_rs::{chmin, debug, debug2D};
+use proconio::input;
 
-use proconio::{input, marker::{Chars, Bytes, Usize1}};
+const INF: f64 = 1e+20;
 
 fn main() {
-    
-}
+    input! {
+        N: usize,
+        D: usize,
+        W: [f64; N]
+    }
 
-const INF: usize = 1001001001001001001;
+    let ave = W.iter().sum::<f64>() / D as f64;
+
+    debug!(ave);
+
+    // dp[S][i] := 集合Sに含まれる要素をiグループに分割したときの Σ(x_i - x)^2 の最小値
+    let mut dp = vec![vec![INF; D + 1]; 1 << N];
+
+    for S in 0..1 << N {
+        let sum = (0..N)
+            .filter(|&i| S >> i & 1 == 1)
+            .map(|i| W[i])
+            .sum::<f64>();
+
+        dp[S][1] = (sum - ave).powf(2.0);
+    }
+
+    debug2D!(dp);
+
+    for S in 0_usize..1 << N {
+        // 部分集合の列挙
+        for k in 2..=D {
+            dp[S][k] = dp[S][k - 1] + dp[0][1];
+
+            let mut T = S;
+            while T > 0 {
+                chmin! {
+                    dp[S][k],
+                    dp[S - T][k - 1] + dp[T][1]
+                };
+                T = (T - 1) & S;
+            }
+        }
+    }
+
+    debug2D!(dp);
+
+    let ans = dp[(1 << N) - 1][D] / D as f64;
+
+    println!("{ans}");
+}
