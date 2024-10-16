2dbce5a62ab9e16fd320b00b88f2d8ce88e498ad
kentakom1213 <kentakom1213@gmail.com>
2024-10-16 23:20:44 +0900

    [add] f問題

--- contests/abc363/src/bin/a.rs
+++ contests/abc363/src/bin/a.rs
@@ -1,6 +1,19 @@
 #![allow(non_snake_case)]
 
-fn main() {
-    
-}
+use proconio::input;
 
+fn main() {
+    input! {
+        R: isize,
+    }
+
+    let res = if R <= 99 {
+        100 - R
+    } else if R <= 199 {
+        200 - R
+    } else {
+        300 - R
+    };
+
+    println!("{res}");
+}
--- contests/abc363/src/bin/b.rs
+++ contests/abc363/src/bin/b.rs
@@ -1,6 +1,24 @@
 #![allow(non_snake_case)]
 
-fn main() {
-    
-}
+use cp_library_rs::debug;
+use proconio::input;
 
+fn main() {
+    input! {
+        N: usize,
+        T: usize,
+        P: usize,
+        mut L: [usize; N]
+    }
+
+    L.sort();
+    L.reverse();
+
+    // 後ろからP番目の人
+    let target = L[P - 1];
+    debug!(target);
+
+    let res = T.saturating_sub(target);
+
+    println!("{res}");
+}
