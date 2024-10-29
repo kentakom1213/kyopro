631e005962b375b4605477254327faac5f50681d
kentakom1213 <kentakom1213@gmail.com>
2024-10-29 16:39:52 +0900

    [add] 青diff

--- contests/abc348/src/bin/f.rs
+++ contests/abc348/src/bin/f.rs
@@ -1,7 +1,44 @@
-#![allow(non_snake_case)]
+use cp_library_rs::debug;
+use fixedbitset::FixedBitSet;
+use proconio::input;
 
-const INF: usize = 1001001001001001001;
+#[allow(non_snake_case)]
 
 fn main() {
-    
+    input! {
+        N: usize,
+        M: usize,
+        A: [[usize; M]; N]
+    }
+
+    let mut bt = vec![FixedBitSet::with_capacity(MAX_N); N];
+    let mut bs = vec![FixedBitSet::with_capacity(MAX_N); 1000];
+
+    for i in 0..M {
+        for j in 0..N {
+            bs[A[j][i]].set(j, true);
+        }
+        for j in 0..N {
+            bt[j] ^= &bs[A[j][i]];
+        }
+        for j in 0..N {
+            bs[A[j][i]].set(j, false);
+        }
+    }
+
+    let mut ans = 0;
+
+    for i in 0..N {
+        ans += bt[i].count_ones(..);
+    }
+
+    debug!(ans);
+
+    if M & 1 == 1 {
+        ans -= N;
+    }
+
+    println!("{}", ans / 2);
 }
+
+const MAX_N: usize = 2000;
