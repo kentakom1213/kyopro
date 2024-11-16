1608ed3acbcd3a9dd2a50abcbea1c0d0d887f1bf
kentakom1213 <kentakom1213@gmail.com>
2024-11-16 10:29:07 +0900

    [add] 典型90

--- contests/typical90/src/bin/019.rs
+++ contests/typical90/src/bin/019.rs
@@ -1,26 +1,38 @@
-// attributes
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 #![allow(non_snake_case)]
-#![allow(dead_code)]
-#![allow(unused_macros)]
 
-// imports
-use itertools::Itertools;
-use proconio::{input, marker::{Chars, Bytes, Usize1}};
-
-macro_rules! debug {
-    ( $($val:expr),* $(,)* ) => {{
-        #[cfg(debug_assertions)]
-        eprintln!( concat!($(stringify!($val), " = {:?}, "),*), $($val),* );
-    }};
-}
-
-// constant
-const MOD1: usize = 1_000_000_007;
-const MOD9: usize = 998_244_353;
-const INF: usize = 1001001001001001001;
+use cp_library_rs::{chmin, utils::consts::INF};
+use memoise::memoise;
+use proconio::input;
 
 fn main() {
-    
+    input! {
+        N: usize,
+        A: [usize; N * 2]
+    }
+
+    let ans = dp(0, 2 * N, &A);
+
+    println!("{ans}");
+}
+
+#[memoise(l <= 404, r <= 404)]
+fn dp(l: usize, r: usize, A: &[usize]) -> usize {
+    assert!(l < r, "{l} >= {r}");
+    assert!((r - l) % 2 == 0, "{l}, {r}");
+
+    if l + 2 == r {
+        return A[l].abs_diff(A[l + 1]);
+    }
+
+    let mut ans = INF;
+
+    // 外側から取る
+    chmin!(ans, A[l].abs_diff(A[r - 1]) + dp(l + 1, r - 1, A));
+
+    // ブロックに分割
+    for i in (l + 2..r).step_by(2) {
+        chmin!(ans, dp(l, i, A) + dp(i, r, A));
+    }
+
+    ans
 }
