33dd36daecd032db2501aac519823741a47cdfb5
kentakom1213 <kentakom1213@gmail.com>
2024-11-18 17:04:23 +0900

    [add] G復習

--- contests/abc380/src/bin/g.rs
+++ contests/abc380/src/bin/g.rs
@@ -1,3 +1,65 @@
 #![allow(non_snake_case)]
 
-fn main() {}
+use cp_library_rs::{
+    algebraic_structure::operation::Add, data_structure::segment_tree::SegmentTree, debug,
+    number_theory::modint::M998, utils::num_traits::Zero,
+};
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        N: usize,
+        K: usize,
+        P: [Usize1; N]
+    }
+
+    // 数列全体の転倒数
+    let inv_all = {
+        let mut seg = SegmentTree::<Add<M998>>::new(N);
+        let mut res = M998::zero();
+        for &p in &P {
+            res += seg.get_range(p..);
+            *seg.get_mut(p).unwrap() += 1;
+        }
+        res
+    };
+
+    // 長さKの区間の転倒数の期待値
+    let exp_k = M998::new(K) * (K - 1) / 2 / 2;
+
+    // 長さKの区間全部について考える
+    let mut ans = M998::zero();
+    let mut seg = SegmentTree::<Add<M998>>::new(N);
+    let mut inv = M998::zero();
+
+    // 0..K で初期化
+    for &p in &P[..K] {
+        inv += seg.get_range(p..);
+        *seg.get_mut(p).unwrap() += 1;
+    }
+
+    debug!(seg);
+    debug!(inv);
+
+    ans += inv_all - inv + exp_k;
+
+    // ずらす
+    for i in 0..N - K {
+        let l = P[i];
+        *seg.get_mut(l).unwrap() -= 1;
+        inv -= seg.get_range(..l);
+
+        let r = P[i + K];
+        inv += seg.get_range(r..);
+        *seg.get_mut(r).unwrap() += 1;
+
+        debug!(seg);
+        debug!(inv, l, r);
+
+        ans += inv_all - inv + exp_k;
+    }
+
+    ans /= N - K + 1;
+
+    println!("{ans}");
+}
