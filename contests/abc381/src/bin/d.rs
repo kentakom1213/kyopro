0ed180c96e0aa5de10bab4f9d138c0f41ab90e22
kentakom1213 <kentakom1213@gmail.com>
2024-11-23 00:08:25 +0900

    [add] D問題通す

--- contests/abc381/src/bin/d.rs
+++ contests/abc381/src/bin/d.rs
@@ -3,63 +3,74 @@
 use std::collections::VecDeque;
 
 use cp_library_rs::{chmax, debug, utils::run_length::RunLength};
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
     input! {
         N: usize,
-        A: [usize; N]
+        A: [usize; N],
     }
+
+    let A = A.into_iter().run_length_encode();
 
     let mut ans = 0;
 
-    let rle = A.iter().cloned().run_length_encode();
-
-    let mut tmp = vec![];
+    let mut deq = VecDeque::new();
     let mut set = vec![false; N + 1];
 
-    for (a, n) in rle {
+    let clear_set = |set: &mut Vec<bool>, deq: &mut VecDeque<usize>| {
+        while let Some(x) = deq.pop_front() {
+            set[x] = false;
+        }
+    };
+
+    for (a, n) in A {
         if set[a] {
-            chmax!(ans, tmp.len());
-
-            // 削除
-            while let Some(x) = tmp.pop() {
-                set[x] = false;
-            }
-
-            if n >= 2 {
-                tmp.push(a);
+            if n == 1 {
+                // クリア
+                clear_set(&mut set, &mut deq);
+            } else if n == 2 {
+                // 条件を満たすまで削除
+                while let Some(x) = deq.pop_front() {
+                    if x == a {
+                        set[a] = false;
+                        break;
+                    }
+                    set[x] = false;
+                }
+                deq.push_back(a);
+                set[a] = true;
+            } else {
+                // クリア
+                clear_set(&mut set, &mut deq);
+                // 再度追加
+                deq.push_back(a);
                 set[a] = true;
             }
         } else {
-            if n < 2 {
-                chmax!(ans, tmp.len());
-                while let Some(x) = tmp.pop() {
-                    set[x] = false;
-                }
+            if n == 1 {
+                // クリア
+                clear_set(&mut set, &mut deq);
             } else if n == 2 {
-                tmp.push(a);
+                // 追加
+                deq.push_back(a);
                 set[a] = true;
-                chmax!(ans, tmp.len());
             } else {
-                tmp.push(a);
+                // 追加
+                deq.push_back(a);
                 set[a] = true;
-
-                chmax!(ans, tmp.len());
-
-                // 削除
-                while let Some(x) = tmp.pop() {
-                    set[x] = false;
-                }
-
+                chmax!(ans, deq.len());
+                // クリア
+                clear_set(&mut set, &mut deq);
                 // 再度追加
-                tmp.push(a);
+                deq.push_back(a);
                 set[a] = true;
             }
         }
+        chmax!(ans, deq.len());
+        debug!(deq);
     }
-
-    chmax!(ans, tmp.len());
+    chmax!(ans, deq.len());
 
     ans *= 2;
 
