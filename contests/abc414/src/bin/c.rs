4586fa09e94256a78c1ee5a785dd3c2ff0d08dd3
kentakom1213 <kentakom1213@gmail.com>
2025-07-14 01:04:20 +0900

    add: ライブラリ化

--- contests/abc414/src/bin/c.rs
+++ contests/abc414/src/bin/c.rs
@@ -1,7 +1,10 @@
 #![allow(non_snake_case)]
 
+use cp_library_rs::{
+    debug,
+    utils::palindrome::{generate_palindrome_number, is_palindrome},
+};
 use proconio::input;
-use rustc_hash::FxHashSet;
 
 fn main() {
     input! {
@@ -9,70 +12,9 @@ fn main() {
         N: usize
     }
 
-    let Nstr = N.to_string();
-    let Nhalf = Nstr[..Nstr.len() / 2 + 1].parse::<usize>().unwrap();
-
-    // debug!(Nhalf);
-
-    let mut ans = 0;
-
-    for p in 1..=9 {
-        if p > N {
-            break;
-        }
-        if is_palin(p, A) {
-            ans += p;
-        }
-    }
-
-    for i in 1..=Nhalf {
-        let istr = i.to_string();
-        let irev = istr.chars().rev().collect::<String>();
-        let pstr = istr + &irev;
-        let p = pstr.parse::<usize>().unwrap();
-
-        if p > N {
-            break;
-        }
-
-        if is_palin(p, A) {
-            ans += p;
-        }
-    }
-
-    'outer: for i in 1..=Nhalf {
-        for o in '0'..='9' {
-            let istr = i.to_string();
-            let irev = istr.chars().rev().collect::<String>();
-            let pstr = istr + &o.to_string() + &irev;
-            let p = pstr.parse::<usize>().unwrap();
-
-            if p > N {
-                break 'outer;
-            }
-
-            if is_palin(p, A) {
-                ans += p;
-            }
-        }
-    }
+    let ans: usize = generate_palindrome_number(N)
+        .filter(|&n| is_palindrome(n, A))
+        .sum();
 
     println!("{ans}");
-}
-
-fn is_palin(n: usize, a: usize) -> bool {
-    let mut ps = vec![];
-
-    let mut x = n;
-    while x > 0 {
-        ps.push(x % a);
-        x /= a;
-    }
-
-    let ps2 = ps.clone();
-    ps.reverse();
-
-    // debug!(n, ps, ps2);
-
-    ps == ps2
 }
