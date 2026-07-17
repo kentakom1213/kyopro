ad0e6fda3433ee1992b530a84224d5d890ffb409
kentakom1213 <kentakom1213@gmail.com>
2026-07-17 16:45:20 +0900

    add: 平面走査

--- contests/dp/src/bin/q.rs
+++ contests/dp/src/bin/q.rs
@@ -1,24 +1,28 @@
-// attributes
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 #![allow(non_snake_case)]
-#![allow(dead_code)]
-#![allow(unused_macros)]
 
-// imports
-use itertools::Itertools;
-use proconio::{
-    input,
-    marker::{Bytes, Chars, Usize1},
+use cp_library_rs::{
+    algebraic_structure::operation::Max, data_structure::segment_tree::SegmentTree, debug,
 };
+use proconio::input;
 
-macro_rules! debug {
-    ( $($val:expr),* $(,)* ) => {{
-        #[cfg(debug_assertions)]
-        eprintln!( concat!($(stringify!($val), " = {:?}, "),*), $($val),* );
-    }};
+fn main() {
+    input! {
+        N: usize,
+        H: [usize; N],
+        A: [usize; N],
+    }
+
+    // dp[h] := 末尾に高さ h の花があるときの美しさの総和の最大値
+    let mut dp = SegmentTree::<Max<usize>>::new(N + 1);
+
+    for i in 0..N {
+        let max = dp.get_range(..H[i]);
+        *dp.get_mut(H[i]).unwrap() = max + A[i];
+
+        debug!(dp);
+    }
+
+    let ans = dp.get_range(..);
+
+    println!("{ans}");
 }
-
-fn main() {}
-
-const INF: usize = 1001001001001001001;
